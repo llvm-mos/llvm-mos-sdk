533cfe007ebf110d14e3149d20d1a4333fe7a6c1
Daniel Thornburgh <mysterymath@gmail.com>
2021-03-15 18:08:05 -0700

    Add C64 target to start with. Basic header only.

---
+++ c64/basic_header.s
@@ -0,0 +1,8 @@
+; SYS 2061 in C64 BASIC
+.macro basic_sys_command
+	.byte 0x9e   ; SYS keyword BASIC token
+	.asciz "2061" ; the location of the _start symbol, in ASCII
+.endm
+
+.include "common/lib/basic_header.s"
+

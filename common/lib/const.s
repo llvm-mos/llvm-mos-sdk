c46a9fd2016d8facf68ba475c6d85040d5eefed1
Daniel Thornburgh <mysterymath@gmail.com>
2021-05-11 23:00:24 -0700

    Add __set_v constant to allow BIT __set_v to set V.

---
+++ common/lib/const.s
@@ -0,0 +1,10 @@
+; Fixed constants used by the compiler in ways that preclude immediate
+; addressing.
+
+.data
+
+; Location that when used with BIT will set V. Compensates for the lack of a SEV
+; instruction.
+.global __set_v
+__set_v:
+  .byte 0xFF

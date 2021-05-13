958675232211ec28566ed40d259cbb8f0f54d06e
Daniel Thornburgh <mysterymath@gmail.com>
2021-05-13 15:34:02 -0700

    Initialize soft stack pointer.

--- common/lib/crt0.s
+++ common/lib/crt0.s
@@ -1,5 +1,11 @@
 .section .start,"ax",@progbits
 .global _start
 _start:
-    ; Eventually this will zero BSS, set up the soft stack, etc.
+    ; Initialize soft stack pointer.
+    LDA #mos16lo(__stack)
+    STA mos8(__rc0)
+    LDA #mos16hi(__stack)
+    STA mos8(__rc1)
+
+    ; FIXME: Zero BSS.
     JMP main

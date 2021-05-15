b1e13a5a58b2450f1d35c2882e0db341428ebeb3
Daniel Thornburgh <mysterymath@gmail.com>
2021-05-14 18:09:39 -0700

    Clean up __memset.

--- common/lib/mem.c
+++ common/lib/mem.c
@@ -5,8 +5,7 @@
 // This is also generally the best version of memset on MOS. All non-pointer
 // arguments can fit in registers, and there is no superfluous return value.
 void __memset(char *ptr, char value, size_t num) {
-  char *cur = (char*)ptr;
-  while (num--) *cur++ = value;
+  while (num--) *ptr++ = value;
 }
 
 void* memset(void *ptr, int value, size_t num) {

604c564321eee99cd02f080de3b74a801a3b8f48
Daniel Thornburgh <mysterymath@gmail.com>
2021-05-19 08:28:15 -0700

    Use volatile pointer to simulator IO  register to ensure writes occur.

--- sim/lib/putchar.c
+++ sim/lib/putchar.c
@@ -3,5 +3,5 @@
 void __putchar(char c) {
   // Writing to this IO register causes the simulator to print the written byte
   // as a character to stdout.
-  (*(char *)0xFFF9) = c;
+  (*(volatile char *)0xFFF9) = c;
 }

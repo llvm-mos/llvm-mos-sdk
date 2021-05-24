ba6888ce86f1a982573075bf84450ebc7b78c6cc
Mariusz Kryński <mrk@sed.pl>
2021-05-24 23:24:56 +0200

    use atari os vector table

--- atari/800xl/lib/chrout.c
+++ atari/800xl/lib/chrout.c
@@ -1,5 +1,12 @@
 void __chrout(char c) {
   if(c == '\n') c = 0x9b;
   __attribute__((leaf)) asm volatile(
-    "jsr\t$F2B0\n" : "+a"(c) : : "p", "x", "y");
+    "tax\n"
+    "lda $e407\n"
+    "pha\n"
+    "lda $e406\n"
+    "pha\n"
+    "txa\n"
+    "rts\n"
+    : "+a"(c) : : "p", "x", "y");
 }

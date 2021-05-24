1c628518641a7414326b57c5518b59bc61bf27b8
Daniel Thornburgh <mysterymath@gmail.com>
2021-05-23 22:59:59 -0700

    Implement memcpy.

--- common/lib/mem.c
+++ common/lib/mem.c
@@ -1,9 +1,15 @@
 #include <string.h>
 
-// Implement the libcall produced by lowering G_MEMSET operation.
-//
-// This is also generally the best version of memset on MOS. All non-pointer
-// arguments can fit in registers, and there is no superfluous return value.
+void* memcpy(void *dest, const void *src, size_t count) {
+  char *d = dest;
+  const char *s = src;
+  while(count--) *d++ = *s++;
+  return dest;
+}
+
+// Libcall for G_MEMSET. This is also generally the best version of memset on
+// MOS. All non-pointer arguments can fit in registers, and there is no
+// superfluous return value.
 void __memset(char *ptr, char value, size_t num) {
   while (num--) *ptr++ = value;
 }

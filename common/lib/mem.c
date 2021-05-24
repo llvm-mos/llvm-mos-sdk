19894e897286fd5869a33944d12a0a47e519f2f1
Daniel Thornburgh <mysterymath@gmail.com>
2021-05-23 23:12:07 -0700

    Make __memset extern C {}

--- common/lib/mem.c
+++ common/lib/mem.c
@@ -7,14 +7,11 @@ void* memcpy(void *dest, const void *src, size_t count) {
   return dest;
 }
 
-// Libcall for G_MEMSET. This is also generally the best version of memset on
-// MOS. All non-pointer arguments can fit in registers, and there is no
-// superfluous return value.
-void __memset(char *ptr, char value, size_t num) {
-  while (num--) *ptr++ = value;
-}
-
 void* memset(void *ptr, int value, size_t num) {
   __memset((char*)ptr, (char)value, num);
   return ptr;
+}
+
+void __memset(char *ptr, char value, size_t num) {
+  while (num--) *ptr++ = value;
 }

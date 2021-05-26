c389a546bb518c2670566b6b89379368ccc7fa6a
Daniel Thornburgh <mysterymath@gmail.com>
2021-05-26 15:06:15 -0700

    Implement remaining multiplication libcalls (in C++!)

--- cmake/modules/CompileForMOS.cmake
+++ cmake/modules/CompileForMOS.cmake
@@ -4,9 +4,11 @@ include_directories(${CMAKE_SOURCE_DIR}/common/include)
 
 set(CMAKE_ASM_COMPILER "${LLVM_MOS}/clang" PARENT_SCOPE)
 set(CMAKE_C_COMPILER "${LLVM_MOS}/clang" PARENT_SCOPE)
+set(CMAKE_CXX_COMPILER "${LLVM_MOS}/clang" PARENT_SCOPE)
 set(CMAKE_AR "${LLVM_MOS}/llvm-ar" PARENT_SCOPE)
 set(CMAKE_RANLIB "${LLVM_MOS}/llvm-ranlib" PARENT_SCOPE)
 
 set(CMAKE_ASM_FLAGS "--target=mos" PARENT_SCOPE)
 set(CMAKE_C_FLAGS "--target=mos -flto -Os" PARENT_SCOPE)
+set(CMAKE_CXX_FLAGS "--target=mos -flto -Os" PARENT_SCOPE)
 endfunction()

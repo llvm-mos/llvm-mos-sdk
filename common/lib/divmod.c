0b768e944b915ec5fb7294f079b8a06e611117a4
Daniel Thornburgh <mysterymath@gmail.com>
2021-05-28 11:27:04 -0700

    Add faster division algorithm.

--- common/lib/divmod.c
+++ common/lib/divmod.c
@@ -1,7 +1,53 @@
-// These are just temporary; writing a good division algorithm is quite
-// difficult, and branchless off-the-shelf ones are likely to do quite poorly
-// on the 6502. We should iron out as many compiler bugs as we can before
-// tuning division, since it's sure to be a grueling process.
+#ifndef __SLOW_DIV
+
+// Relatively straigtforward implementation of long division in C. Not
+// particularly tuned for performance, but clear.
+
+unsigned long __udivsi3(unsigned long a, unsigned long b) {
+  if (!b)
+    return 0;
+
+  unsigned long long divisor = b;
+  divisor <<= 32;
+
+  unsigned long q = 0;
+  for (char i = 0; i < 32; ++i) {
+    divisor >>= 1;
+
+    q <<= 1;
+    if (divisor <= a) {
+      a -= divisor;
+      q |= 1;
+    }
+  }
+
+  return q;
+}
+
+unsigned long __umodsi3(unsigned long a, unsigned long b) {
+  if (!b)
+    return a;
+
+  unsigned long long divisor = b;
+  divisor <<= 32;
+
+  for (char i = 0; i < 32; ++i) {
+    divisor >>= 1;
+
+    if (divisor <= a)
+      a -= divisor;
+  }
+
+  return a;
+}
+
+
+#else  // __SLOW_DIV
+
+
+// Very slow versions of the division algorithm. Still useful for validating
+// whether or not breakages are likely to be caused by a miscompile of the
+// division algorithm.
 
 unsigned long __udivsi3(unsigned long a, unsigned long b) {
   unsigned long q = 0;
@@ -17,3 +63,6 @@ unsigned long __umodsi3(unsigned long a, unsigned long b) {
     a -= b;
   return a;
 }
+
+
+#endif  // __SLOW_DIV

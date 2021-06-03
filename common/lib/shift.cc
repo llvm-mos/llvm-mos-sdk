5ef3f76de71243efab85088dab4470749ede8152
Daniel Thornburgh <mysterymath@gmail.com>
2021-06-03 09:06:06 -0700

    Add arithmetic right shift libcalls.

--- common/lib/shift.cc
+++ common/lib/shift.cc
@@ -1,27 +1,40 @@
-template <typename T> static inline T lsh(T n, char amt) {
+template <typename T> static inline T shl(T n, char amt) {
   while (amt--)
     n <<= 1;
   return n;
 }
 
-template <typename T> static inline T shr(T n, char amt) {
+template <typename T> static inline T lshr(T n, char amt) {
   while (amt--)
     n >>= 1;
   return n;
 }
 
-extern "C" {
-char __ashlqi3(char n, char amt) { return lsh(n, amt); }
-unsigned int __ashlhi3(unsigned int n, char amt) { return lsh(n, amt); }
-unsigned long __ashlsi3(unsigned long n, char amt) { return lsh(n, amt); }
-unsigned long long __ashldi3(unsigned long long n, char amt) {
-  return lsh(n, amt);
+template <typename T> static inline T ashr(T n, char amt) {
+  while (amt--)
+    n = n >> 1 | n & (T)1 << (sizeof(T) * 8 - 1);
+  return n;
 }
 
-char __lshrqi3(char n, char amt) { return shr(n, amt); }
-unsigned int __lshrhi3(unsigned int n, char amt) { return shr(n, amt); }
-unsigned long __lshrsi3(unsigned long n, char amt) { return shr(n, amt); }
+extern "C" {
+char __ashlqi3(char n, char amt) { return shl(n, amt); }
+unsigned int __ashlhi3(unsigned int n, char amt) { return shl(n, amt); }
+unsigned long __ashlsi3(unsigned long n, char amt) { return shl(n, amt); }
+unsigned long long __ashldi3(unsigned long long n, char amt) {
+  return shl(n, amt);
+}
+
+char __lshrqi3(char n, char amt) { return lshr(n, amt); }
+unsigned int __lshrhi3(unsigned int n, char amt) { return lshr(n, amt); }
+unsigned long __lshrsi3(unsigned long n, char amt) { return lshr(n, amt); }
 unsigned long long __lshrdi3(unsigned long long n, char amt) {
-  return shr(n, amt);
+  return lshr(n, amt);
+}
+
+char __ashrqi3(char n, char amt) { return ashr(n, amt); }
+unsigned int __ashrhi3(unsigned int n, char amt) { return ashr(n, amt); }
+unsigned long __ashrsi3(unsigned long n, char amt) { return ashr(n, amt); }
+unsigned long long __ashrdi3(unsigned long long n, char amt) {
+  return ashr(n, amt);
 }
 }

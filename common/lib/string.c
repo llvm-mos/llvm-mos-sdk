ba3d4d768ef57300ee7ad04b45082583442aa067
Daniel Thornburgh <mysterymath@gmail.com>
2021-06-05 12:58:18 -0700

    Implement strncmp.

--- common/lib/string.c
+++ common/lib/string.c
@@ -17,7 +17,16 @@ int strcmp(const char *s1, const char *s2) {
   }
 }
 
-char* strcpy(char *restrict s1, const char *restrict s2) {
+int strncmp(const char *s1, const char *s2, size_t n) {
+  for (;; ++s1, ++s2, --n) {
+    if (!n)
+      return 0;
+    if (!*s1 || !*s2 || *s1 != *s2)
+      return *s1 - *s2;
+  }
+}
+
+char *strcpy(char *restrict s1, const char *restrict s2) {
   char *ret = s1;
   for (;; ++s1, ++s2) {
     *s1 = *s2;

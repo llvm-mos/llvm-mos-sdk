054cd4e0e9072100b06137bb6c2d2506480d7fc7
Daniel Thornburgh <mysterymath@gmail.com>
2021-06-05 13:06:25 -0700

    Implement memcmp.

--- common/include/string.h
+++ common/include/string.h
@@ -8,23 +8,24 @@
 extern "C" {
 #endif
 
+int memcmp(const void *s1, const void *s2, size_t n);
 void *memcpy(void *dest, const void *src, size_t count);
-
 void *memset(void *ptr, int value, size_t num);
+
+char *strchr(const char *s, int c);
+
+int strcmp(const char *s1, const char *s2);
+
+char* strcpy(char *restrict s1, const char *restrict s2);
+
+size_t strlen(const char *s);
+int strncmp(const char *s1, const char *s2, size_t n);
+
 
 // Version of memset with better arguments for MOS. All non-pointer arguments
 // can fit in registers, and there is no superfluous return value. Compiler
 // intrinsic memset calls use this version, and user code is free to as well.
 void __memset(char *ptr, char value, size_t num);
-
-char *strchr(const char *s, int c);
-
-int strcmp(const char *s1, const char *s2);
-int strncmp(const char *s1, const char *s2, size_t n);
-
-char* strcpy(char *restrict s1, const char *restrict s2);
-
-size_t strlen(const char *s);
 
 #ifdef __cplusplus
 }

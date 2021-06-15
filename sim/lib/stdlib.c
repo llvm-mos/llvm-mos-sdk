0a0404ec575ed1f5b6d8f5dc69c6260e6ed0349c
Daniel Thornburgh <mysterymath@gmail.com>
2021-06-15 10:04:16 -0700

    Prevent compiler from overzealous control-flow inference in abort() and exit().

--- sim/lib/stdlib.c
+++ sim/lib/stdlib.c
@@ -3,11 +3,19 @@
 void abort(void) {
   // Writing to this IO register causes the simulator to abort.
   (*(volatile char *)0xFFF7) = 1;
+
+  // Prevent the compiler from considering this entire basic block unreachable.
+  asm volatile ("");
+
   __builtin_unreachable();
 }
 
 void exit(int status) {
   // Writing to this IO register causes the simulator to exit with the given status.
   (*(volatile char *)0xFFF8) = (char)status;
+
+  // Prevent the compiler from considering this entire basic block unreachable.
+  asm volatile ("");
+
   __builtin_unreachable();
 }

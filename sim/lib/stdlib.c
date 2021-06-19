e7ac9d63b52e1090fd5c72f1da238ce2aa4ed66f
Mariusz Kryński <mrk@sed.pl>
2021-06-19 22:36:20 +0200

    Implement clock / reset_clock for sim

--- sim/lib/stdlib.c
+++ sim/lib/stdlib.c
@@ -19,3 +19,17 @@ void exit(int status) {
 
   __builtin_unreachable();
 }
+
+unsigned long clock() {
+  // reading first byte latches whole 32-bit value
+  // so it needs to be read byte by byte in correct order
+  unsigned long ticks;
+  for(int i=0; i < 4; i++) {
+    ((unsigned char *)&ticks)[i] = *((volatile unsigned char *)0xFFF0 + i);
+  }
+  return ticks;
+}
+
+void reset_clock() {
+  *((volatile unsigned char *)0xFFF0) = 0;
+}

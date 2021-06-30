dfc0964263734d3f419091fe49eb082f364db03a
Daniel Thornburgh <mysterymath@gmail.com>
2021-06-30 16:40:01 -0700

    Implement memmove.

--- common/lib/mem.c
+++ common/lib/mem.c
@@ -3,9 +3,9 @@
 void *memchr(const void *s, int c, size_t n) {
   const char *sc;
   char ch;
-  for (sc = (const char*)s, ch = (char)c; n; sc++, n--)
+  for (sc = (const char *)s, ch = (char)c; n; sc++, n--)
     if (*sc == ch)
-      return (void*)sc;
+      return (void *)sc;
   return NULL;
 }
 
@@ -17,7 +17,7 @@ int memcmp(const void *s1, const void *s2, size_t n) {
   return 0;
 }
 
-void* memcpy(void *dest, const void *src, size_t count) {
+void *memcpy(void *dest, const void *src, size_t count) {
   char *d;
   const char *s;
   for (d = dest, s = src; count; d++, s++, --count)
@@ -25,12 +25,35 @@ void* memcpy(void *dest, const void *src, size_t count) {
   return dest;
 }
 
-void* memset(void *ptr, int value, size_t num) {
-  __memset((char*)ptr, (char)value, num);
+void *memset(void *ptr, int value, size_t num) {
+  __memset((char *)ptr, (char)value, num);
   return ptr;
 }
 
 void __memset(char *ptr, char value, size_t num) {
   for (; num; ptr++, num--)
     *ptr = value;
+}
+
+void *memmove(void *dest, const void *src, size_t num) {
+  unsigned long udest = (unsigned long)dest;
+  unsigned long usrc = (unsigned long)src;
+  if (udest <= usrc)
+    return memcpy(dest, src, num);
+
+  // Don't add -1 to dest or src; this is undefined behavior.
+  if (!num)
+    return dest;
+  char *d = dest + num - 1;
+  const char *s = src + num - 1;
+
+  while (1) {
+    *d = *s;
+    // Don't decrement d or s past the beginning of the object; this is
+    // undefined behavior.
+    if (!--num)
+      return dest;
+    --d, --s;
+  }
+  return dest;
 }

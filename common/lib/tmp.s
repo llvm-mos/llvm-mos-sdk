4bf6a9d5e20e3952f9407f982e751c5230f9ffd9
Daniel Thornburgh <mysterymath@gmail.com>
2021-08-16 18:31:39 -0700

    Revert "Remove __save_x and __save_y."
    This reverts commit 9a6f03689c9f39ae32ee00f4f4b2eef1f18f36d1.
    
    Breaks end-to-end test suite.

--- common/lib/tmp.s
+++ common/lib/tmp.s
@@ -8,6 +8,16 @@
 __save_a:
     .space 1
 
+; Location to temporarily save the value of the X register.
+.global __save_x
+__save_x:
+    .space 1
+
+; Location to temporarily save the value of the Y register.
+.global __save_y
+__save_y:
+    .space 1
+
 ; Pointer containing target of indirect call.
 .align 2 ; Prevent NMOS bug when the pointer is on a page boundary.
 .global __call_indir_target

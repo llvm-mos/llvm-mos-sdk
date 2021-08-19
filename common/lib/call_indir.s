fe532f5aa27ed19ede5efd5dcb3b47a849c4e6b7
Daniel Thornburgh <mysterymath@gmail.com>
2021-08-18 20:04:47 -0700

    Replace old indirect call libcall with new.

--- common/lib/call_indir.s
+++ common/lib/call_indir.s
@@ -1,19 +1,9 @@
 .text
-; Function that performs an indirect call through the pointer
-; __call_indir_target. JSRing to this routine effectively performs an indirect
-; JSR, since the RTS in the callee will return to the original JSR location,
-; ignoring this stub. All arguments and return values are trivially forwarded.
-; TODO: Remove this version in favor of __call_indir2
-.global __call_indir
-__call_indir:
-  jmp (__call_indir_target)
-
 ; Function that performs an indirect call through the pointer in RS4. JSRing to
 ; this routine effectively performs an indirect JSR, since the RTS in the callee
 ; will return to the original JSR location, ignoring this stub. All arguments
 ; and return values are trivially forwarded (RS4 is callee-saved and can never
 ; contain arguments).
-; TODO: Rename this to __call_indir
-.global __call_indir2
-__call_indir2:
+.global __call_indir
+__call_indir:
   jmp (__rc8)

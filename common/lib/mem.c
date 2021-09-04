bfd8bb6f6eb89661f89a86d60add2cb38984dfa2
Daniel Thornburgh <mysterymath@gmail.com>
2021-09-04 14:31:00 -0700

    Use 16-bit unsigned ints in memmove.

--- common/lib/mem.c
+++ common/lib/mem.c
@@ -36,8 +36,8 @@ void __memset(char *ptr, char value, size_t num) {
 }
 
 void *memmove(void *dest, const void *src, size_t num) {
-  unsigned long udest = (unsigned long)dest;
-  unsigned long usrc = (unsigned long)src;
+  unsigned int udest = (unsigned int)dest;
+  unsigned int usrc = (unsigned int)src;
   if (udest <= usrc)
     return memcpy(dest, src, num);
 

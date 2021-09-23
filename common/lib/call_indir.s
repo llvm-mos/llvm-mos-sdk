99bcfd9e58671a4dff542b5d0f118d9873e1a5eb
Daniel Thornburgh <mysterymath@gmail.com>
2021-09-23 08:11:56 -0700

    Use RS8 for call-indir; new RISC-ish calling conv.

--- common/lib/call_indir.s
+++ common/lib/call_indir.s
@@ -1,9 +1,8 @@
 .text
-; Function that performs an indirect call through the pointer in RS4. JSRing to
+; Function that performs an indirect call through the pointer in RS8. JSRing to
 ; this routine effectively performs an indirect JSR, since the RTS in the callee
 ; will return to the original JSR location, ignoring this stub. All arguments
-; and return values are trivially forwarded (RS4 is callee-saved and can never
-; contain arguments).
+; and return values are trivially forwarded (RS8 can never contain arguments).
 .global __call_indir
 __call_indir:
-  jmp (__rc8)
+  jmp (__rc16)

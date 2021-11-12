3df86f214b9cd2b105c30565050510a5e2b91104
Daniel Thornburgh <mysterymath@gmail.com>
2021-11-12 15:45:00 -0800

    Use >>1 in variable ashr libcall.

--- common/lib/shift.cc
+++ common/lib/shift.cc
@@ -4,15 +4,9 @@ template <typename T> static inline T shl(T n, char amt) {
   return n;
 }
 
-template <typename T> static inline T lshr(T n, char amt) {
+template <typename T> static inline T shr(T n, char amt) {
   while (amt--)
     n >>= 1;
-  return n;
-}
-
-template <typename T> static inline T ashr(T n, char amt) {
-  while (amt--)
-    n = n >> 1 | n & (T)1 << (sizeof(T) * 8 - 1);
   return n;
 }
 
@@ -24,17 +18,15 @@ unsigned long long __ashldi3(unsigned long long n, char amt) {
   return shl(n, amt);
 }
 
-char __lshrqi3(char n, char amt) { return lshr(n, amt); }
-unsigned int __lshrhi3(unsigned int n, char amt) { return lshr(n, amt); }
-unsigned long __lshrsi3(unsigned long n, char amt) { return lshr(n, amt); }
+char __lshrqi3(char n, char amt) { return shr(n, amt); }
+unsigned int __lshrhi3(unsigned int n, char amt) { return shr(n, amt); }
+unsigned long __lshrsi3(unsigned long n, char amt) { return shr(n, amt); }
 unsigned long long __lshrdi3(unsigned long long n, char amt) {
-  return lshr(n, amt);
+  return shr(n, amt);
 }
 
-char __ashrqi3(char n, char amt) { return ashr(n, amt); }
-unsigned int __ashrhi3(unsigned int n, char amt) { return ashr(n, amt); }
-unsigned long __ashrsi3(unsigned long n, char amt) { return ashr(n, amt); }
-unsigned long long __ashrdi3(unsigned long long n, char amt) {
-  return ashr(n, amt);
-}
+signed char __ashrqi3(signed char n, char amt) { return shr(n, amt); }
+int __ashrhi3(int n, char amt) { return shr(n, amt); }
+long __ashrsi3(long n, char amt) { return shr(n, amt); }
+long long __ashrdi3(long long n, char amt) { return shr(n, amt); }
 }

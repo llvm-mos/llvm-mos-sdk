c61daabd99e7c84c4d73636358fcfd6fe2b42635
Daniel Thornburgh <mysterymath@gmail.com>
2021-11-17 11:08:50 -0800

    Update call_indir to use RS9.

--- common/lib/call_indir.s
+++ common/lib/call_indir.s
@@ -1,8 +1,8 @@
 .text
-; Function that performs an indirect call through the pointer in RS8. JSRing to
+; Function that performs an indirect call through the pointer in RS9. JSRing to
 ; this routine effectively performs an indirect JSR, since the RTS in the callee
 ; will return to the original JSR location, ignoring this stub. All arguments
-; and return values are trivially forwarded (RS8 can never contain arguments).
+; and return values are trivially forwarded (RS9 can never contain arguments).
 .global __call_indir
 __call_indir:
-  jmp (__rc16)
+  jmp (__rc18)

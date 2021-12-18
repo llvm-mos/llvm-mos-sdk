2847f578281d5b0b2ad3845db96fef083d8b7f67
Kevin Arunski <kevin.arunski@rsa.com>
2021-12-18 17:54:51 -0500

    Simple heap implementation

---
+++ mos-platform/common/lib/stdlib.cc
@@ -0,0 +1,38 @@
+#include "stdlib.h"
+
+#include <new>
+
+extern "C"
+{
+    void *malloc(size_t size)
+    {
+        return new (std::nothrow) std::byte[size];
+    }
+
+    void free(void * ptr)
+    {
+        delete [] static_cast<std::byte *>(ptr);
+    }
+
+    void __memset(char *ptr, char value, size_t num);
+    void *memcpy(void *dest, const void *src, size_t count);
+    
+    void *calloc(size_t num, size_t size)
+    {
+        const auto sz = num*size;
+        const auto block = malloc(sz);
+        
+        if (!block)
+        {
+            return nullptr;
+        }
+
+        __memset(static_cast<char *>(block), 0, sz);
+        return block;
+    }
+
+    void abort()
+    {
+        asm volatile("brk" : );
+    }
+}

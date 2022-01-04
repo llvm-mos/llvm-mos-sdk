be5bd0706a1cb8c64bbfb5aec96d4635cf1be280
Kevin Arunski <kevin.arunski@rsa.com>
2022-01-04 16:53:00 -0500

    Changes requested on PR
    Change C64 abort() implementation to infinite loop.
    Run everything through clang-format --style=LLVM

--- mos-platform/c64/lib/stdlib.c
+++ mos-platform/c64/lib/stdlib.c
@@ -1,9 +1,12 @@
 // place holder for stdlib "abort" function.
+#include <stdio.h>
 
 // Not sure what the best way to handle this is on C64.
 
-void abort()
-{
-    asm volatile("brk" : );
-    __builtin_unreachable();
+void abort() {
+  puts("ABORT() CALLED");
+
+  for (;;) {}
+
+  __builtin_unreachable();
 }

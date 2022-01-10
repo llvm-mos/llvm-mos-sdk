0bd90de665560560b97ec11ff18c4c6de506878f
Mariusz Kryński <mrk@sed.pl>
2022-01-10 19:10:12 +0100

    address reviewer comments

--- mos-platform/common/lib/exit.c
+++ mos-platform/common/lib/exit.c
@@ -1,13 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-extern void __exit(int status);
+extern __attribute__((noreturn)) void __exit(int status);
 
 void exit(int status) {
   __exit(status);
-
-  // Prevent the compiler from considering this entire basic block unreachable.
-  asm volatile ("");
-
-  __builtin_unreachable();
 }

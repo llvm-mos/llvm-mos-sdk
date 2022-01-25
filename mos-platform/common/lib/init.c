de695c28eef75ddd2d0340ed0dc139f59c93ca24
Daniel Thornburgh <mysterymath@gmail.com>
2022-01-25 11:00:02 -0800

    [NFC] Linker script begin -> start, as per convention.

--- mos-platform/common/lib/init.c
+++ mos-platform/common/lib/init.c
@@ -2,20 +2,20 @@
 
 typedef void (*func_ptr)(void);
 
-extern func_ptr __init_array_begin[0], __init_array_end[0];
-extern func_ptr __fini_array_begin[0], __fini_array_end[0];
+extern func_ptr __init_array_start[0], __init_array_end[0];
+extern func_ptr __fini_array_start[0], __fini_array_end[0];
 
-static void __invoke_all(func_ptr * array_begin, func_ptr * array_end) {
-  for (func_ptr *func = array_begin; func != array_end; func++)
+static void __invoke_all(func_ptr * array_start, func_ptr * array_end) {
+  for (func_ptr *func = array_start; func != array_end; func++)
     (*func)();
 }
 
 void _init(void) {
-    __invoke_all(__init_array_begin, __init_array_end);
+    __invoke_all(__init_array_start, __init_array_end);
 }
 
 void _fini(void) {
-    __invoke_all(__fini_array_begin, __fini_array_end);
+    __invoke_all(__fini_array_start, __fini_array_end);
 }
 
 extern int main();

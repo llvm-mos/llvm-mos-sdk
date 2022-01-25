8520ac7d061cc2a24c7ef16d03fdc610622124e4
Daniel Thornburgh <mysterymath@gmail.com>
2022-01-24 22:47:16 -0800

    __exit -> _exit/_Exit, as per C/POSIX std.

--- mos-platform/c64/lib/abort.c
+++ mos-platform/c64/lib/abort.c
@@ -1,9 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-extern __attribute__((noreturn)) void __exit(int status);
+__attribute__((noreturn)) void _exit(int status);
 
 void abort(void) {
    puts("ABORTED");
-   __exit(134);  // 128 + SIGABRT
+   _exit(134);  // 128 + SIGABRT
 }

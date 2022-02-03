84aae2aaca196fca9f367b7fc1131c4575a69b7e
Daniel Thornburgh <mysterymath@gmail.com>
2022-02-02 17:03:51 -0800

    Only zero BSS if necessary.

--- mos-platform/common/lib/zero_bss.c
+++ mos-platform/common/lib/zero_bss.c
@@ -3,13 +3,6 @@
 extern char __bss_start[];
 extern char __bss_end[];
 
-asm (
-  ".section .init.20,\"axR\",@progbits\n"
-  "jsr __do_zero_bss"
-);
-
-// Zero out the BSS region. Targets should reference this symbol unless they
-// automatically ensure that the BSS region is zeroed upon _start.
-void __do_zero_bss(void) {
+void __zero_bss(void) {
   __memset(__bss_start, 0, __bss_end - __bss_start);
 }

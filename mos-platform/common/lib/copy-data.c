638200501f2c716bee678e600ee2e3d6a93921b1
Daniel Thornburgh <mysterymath@gmail.com>
2022-03-24 08:52:34 -0700

    Move simple asm snippets to C files.

--- mos-platform/common/lib/copy-data.c
+++ mos-platform/common/lib/copy-data.c
@@ -1,5 +1,12 @@
 #include <string.h>
 
+asm(
+  ".global __do_copy_data\n"
+  ".section .init.20,\"axR\",@progbits\n"
+  "__do_copy_data:\n"
+  "  jsr __copy_data\n"
+);
+
 extern char __data_start[];
 extern char __data_end[];
 

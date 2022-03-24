ef953bfdce7ba407fff5491083f50b588e5c26cf
Daniel Thornburgh <mysterymath@gmail.com>
2022-03-24 08:52:34 -0700

    Move simple asm snippets to C files.

--- mos-platform/common/lib/zero-bss.c
+++ mos-platform/common/lib/zero-bss.c
@@ -3,6 +3,13 @@
 extern char __bss_start[];
 extern char __bss_end[];
 
+asm(
+  ".global __do_zero_bss\n"
+  ".section .init.20,\"axR\",@progbits\n"
+  "__do_zero_bss:\n"
+  "  jsr __zero_bss\n"
+);
+
 void __zero_bss(void) {
   __memset(__bss_start, 0, __bss_end - __bss_start);
 }

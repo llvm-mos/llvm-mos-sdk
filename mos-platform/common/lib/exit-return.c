1a73e9db7c62c1119f5d5c63e255e60be00189ce
Daniel Thornburgh <mysterymath@gmail.com>
2022-03-24 20:59:34 -0700

    Rename libstart to libcrt0; add docs.

--- mos-platform/common/lib/exit-return.c
+++ mos-platform/common/lib/exit-return.c
@@ -7,7 +7,7 @@ asm (
 
 __attribute__((leaf)) void _fini(void);
 
-// Runs finalizers, then returns to the calleer. Jumping to here after main has
+// Runs finalizers, then returns to the caller. Jumping to here after main has
 // the effect of returning from _start with the given status. When applicable,
 // this can be more efficient than using non-local jump machinery to perform a
 // general _exit.

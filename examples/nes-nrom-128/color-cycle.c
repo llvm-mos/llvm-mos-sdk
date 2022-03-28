89da63c68f9449c6dc815c0bd3da901f9905d217
Daniel Thornburgh <mysterymath@gmail.com>
2022-03-28 14:21:02 -0700

    Add nes-nrom-128 platform.

---
+++ examples/nes-nrom-128/color-cycle.c
@@ -0,0 +1,18 @@
+#include "ppu.h"
+
+int main(void) {
+  // Enable BG rendering.
+  ppu_wait_vblank();
+  PPUMASK = 0b00001000;
+
+  char color = 0;
+  for (;;) {
+    // Wait for 0.5 second.
+    for (int i = 0; i < 30; ++i)
+      ppu_wait_vblank();
+
+    // Increment the palette color 0.
+    ppu_write_addr(0x3f00);
+    PPUDATA = color++;
+  }
+}

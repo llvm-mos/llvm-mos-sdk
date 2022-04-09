5476f553ad33f1987bd6c9088beeca557c96fd5c
Daniel Thornburgh <mysterymath@gmail.com>
2022-04-09 13:28:39 -0700

    Add nes-nrom-256 target.

--- cmake/platform.cmake
+++ cmake/platform.cmake
@@ -1,9 +1,17 @@
 include(install-symlink)
-include(object-file)
 
 function(_check_platform)
   if(NOT PLATFORM)
     message(FATAL_ERROR "platform() was not called in the current scope.")
+  endif()
+endfunction()
+
+function(_check_cross_compiling value)
+  if(CMAKE_CROSSCOMPILING AND NOT value)
+    message(FATAL_ERROR "Host-only function called in target build.")
+  endif()
+  if(NOT CMAKE_CROSSCOMPILING AND value)
+    message(FATAL_ERROR "Target-only function called in host build.")
   endif()
 endfunction()
 
@@ -13,20 +21,24 @@ function(_check_target_prefix target)
   endif()
 endfunction()
 
-function(install_clang_symlinks)
-endfunction()
-
-# Declare a target platform, possibly with the given parent platform. This sets
-# the PLATFORM and PARENT variables accordingly, sets up install directories,
-# and generates a Clang configuration file and corresponding symlinks. The other
+# Declare a target platform, possibly with the given PARENT platform.  The other
 # add_platform_xxx calls require this to have been called in a containing scope.
+#
+# If the platform is declared COMPLETE, then the platform is capable of
+# producing complete binaries; accordingly, a "link.ld" linker script must be
+# present. A HOSTED platform supports the fragment of the C standard library
+# currently implemented. A COMPLETE platform cannot have any descendants.
 function(platform name)
-  cmake_parse_arguments(ARGS "" PARENT "" ${ARGN})
+  cmake_parse_arguments(ARGS "COMPLETE;HOSTED" PARENT "" ${ARGN})
 
   set(PLATFORM ${name})
   set(PLATFORM ${name} PARENT_SCOPE)
   set(PARENT ${ARGS_PARENT})
   set(PARENT ${ARGS_PARENT} PARENT_SCOPE)
+  set(COMPLETE ${ARGS_COMPLETE})
+  set(COMPLETE ${ARGS_COMPLETE} PARENT_SCOPE)
+  set(HOSTED ${ARGS_HOSTED})
+  set(HOSTED ${ARGS_HOSTED} PARENT_SCOPE)
 
   if(NOT CMAKE_CROSSCOMPILING)
     # Clang searches for config files next to itself as a last resort.  If the
@@ -36,20 +48,33 @@ function(platform name)
     foreach(suffix clang clang++ clang-cpp)
       install_symlink(mos-${PLATFORM}-${suffix} mos-${suffix})
     endforeach()
+
+    if(COMPLETE)
+      _add_platform_examples(${PLATFORM}-examples)
+    endif()
     return()
   endif()
 
-  set(CMAKE_INSTALL_LIBDIR mos-platform/${PLATFORM}/lib PARENT_SCOPE)
-  set(CMAKE_INSTALL_INCLUDEDIR mos-platform/${PLATFORM}/include PARENT_SCOPE)
+  set(CMAKE_INSTALL_LIBDIR mos-platform/${PLATFORM}/lib)
+  set(CMAKE_INSTALL_LIBDIR ${CMAKE_INSTALL_LIBDIR} PARENT_SCOPE)
+  set(CMAKE_INSTALL_INCLUDEDIR mos-platform/${PLATFORM}/include)
+  set(CMAKE_INSTALL_INCLUDEDIR ${CMAKE_INSTALL_INCLUDEDIR} PARENT_SCOPE)
 
   # Add Clang configuration file.
   set(clang_config ${CMAKE_CURRENT_BINARY_DIR}/mos-${PLATFORM}.cfg)
+  if(COMPLETE)
+    set(LINKER_SCRIPT "# Use platform linker script.\n-T<CFGDIR>/../mos-platform/${PLATFORM}/lib/link.ld")
+  endif()
   if(PARENT)
     configure_file(${CMAKE_SOURCE_DIR}/mos-platform/derived.cfg ${clang_config})
   else()
     configure_file(${CMAKE_SOURCE_DIR}/mos-platform/root.cfg ${clang_config})
   endif()
   install(FILES ${clang_config} TYPE BIN)
+
+  if(COMPLETE)
+    install(FILES link.ld TYPE LIB)
+  endif()
 endfunction()
 
 # Add a static library to the current platform. The target name must begin with
@@ -57,6 +82,7 @@ endfunction()
 # library has this prefix removed.
 function(add_platform_library target)
   _check_platform()
+  _check_cross_compiling(YES)
   _check_target_prefix(${target})
 
   if(ARGN)
@@ -75,6 +101,7 @@ endfunction()
 # in the library.
 function(add_platform_object_file target output)
   _check_platform()
+  _check_cross_compiling(YES)
   _check_target_prefix(${target})
 
   if(ARGN)
@@ -82,20 +109,19 @@ function(add_platform_object_file target output)
   else()
     add_library(${target} STATIC ${CMAKE_SOURCE_DIR}/mos-platform/stub.c)
   endif()
-  generate_object_file(${target} ${output})
-  install_object_files(${target})
+
+  add_custom_command(OUTPUT ${output}
+    COMMAND ${CMAKE_LINKER} -r -o ${output} --whole-archive
+            $<TARGET_FILE:${target}>)
+  add_custom_target(${target}-o ALL DEPENDS ${output})
+  install(FILES ${CMAKE_CURRENT_BINARY_DIR}/${output} TYPE LIB)
 endfunction()
 
-# Build the example project against the current platform. Any arguments provided
-# are passed as <ARG>=YES flags to the underlying project's CMake configuration
-# step. See the example project for the semantics of various options.
-function(add_platform_examples target)
+# Build the example project against the current platform.
+function(_add_platform_examples target)
   _check_platform()
+  _check_cross_compiling(NO)
   _check_target_prefix(${target})
-
-  foreach(prop ${ARGN})
-    list(APPEND prop_args -D${prop}=YES)
-  endforeach()
 
   get_filename_component(llvm_mos ${LLVM_MOS_C_COMPILER} DIRECTORY)
 
@@ -120,9 +146,9 @@ function(add_platform_examples target)
       -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
       -DLLVM_MOS=${llvm_mos}
       -DPLATFORM=${PLATFORM}
+      -DHOSTED=${HOSTED}
       -DCMAKE_C_FLAGS=${config_flag}
       -DCMAKE_CXX_FLAGS=${config_flag}
-      ${prop_args}
       -DCMAKE_TOOLCHAIN_FILE=${CMAKE_SOURCE_DIR}/cmake/llvm-mos-toolchain.cmake
     BUILD_ALWAYS On
     EXCLUDE_FROM_ALL ${exclude_from_all}

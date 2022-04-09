f2ff35c0e43bc50381923c7ce85aa9de8c980c9f
Daniel Thornburgh <mysterymath@gmail.com>
2022-04-09 16:47:32 -0700

    Add missed file.

---
+++ mos-platform/nes-nrom/ines.s
@@ -0,0 +1,9 @@
+; Defaults for settings in the iNES 2.0 header.
+
+.weak __hard_wired_mirroring
+; Default to horizontal hard-wired mirroring (or N/A)
+__hard_wired_mirroring = 0
+
+.weak __timing
+; Default to RP2C02 ("NTSC NES")
+__timing = 0

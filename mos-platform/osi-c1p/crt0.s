4fd5ae03575017c6161c03b93d14c553c7afc884
Stephan Mühlstrasser <stephan.muehlstrasser@web.de>
2022-06-12 00:36:29 +0200

    getchar() implementation for osi-c1p target (#43)
    * Port keyboard reading function from cc65
    
    * Minor cleanup and clarifications
    
    Put state of keyboard polling routine into dedicated section.
    Adjust size of storage region for keyboard variables.
    
    * Cleanup assembler code
    
    Removed assignments to register X that were made due to
    cc65 calling convention. The only return value used is stored in
    __CHARBUF.
    Always store return value in __CHARBUF.
    
    * More idiomatic assignment of zero-page sections
    
    Improved according to review comment:
    
    https://github.com/llvm-mos/llvm-mos-sdk/pull/43#discussion_r895067819
    
    * Invoke __kbhit via C function call
    
    Improve readability.

--- mos-platform/osi-c1p/crt0.s
+++ mos-platform/osi-c1p/crt0.s
@@ -1,6 +1,8 @@
 ; Assembler crt0 file to clear the screen before the
-; call to main.
+; call to main and to initialize the keyboard reading
+; code.
 
 .section .init.40,"axR",@progbits
+  jsr __initkbhit
   jsr __clrscr
 
--- mos-platform/osi-c1p/getchar.c
+++ mos-platform/osi-c1p/getchar.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int getchar() {
-  puts("getchar() not yet implemented");
-  abort();
+extern void __kbhit(void);
+extern unsigned char volatile __CHARBUF;
+
+int getchar(void)
+{
+    do
+    {
+        __kbhit();
+    } while (!__CHARBUF);
+    
+    int const result = __CHARBUF;
+
+    __CHARBUF = 0;
+
+    return result;
 }

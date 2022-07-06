3b716b05c65a8122417dd6a0d9c890b5f4a5572d
Andrew Noyes <anoyes34@gmail.com>
2022-07-06 12:00:41 -0700

    Initial support for the dodo platform (#49)
    * First attempt at dodo support
    
    Successfully compiles 0xDEADBEEF (see https://play.dodolabs.io/#), but
    doesn't compile any of the other sample games due to missing itoa,
    srand, and rand implementations.
    
    * Add version check
    
    * Fix CLEAR_SPRITE comment formatting
    
    * Fix missing newline at end of file
    
    * Remove redundant #pragma once

---
+++ mos-platform/dodo/crt0.s
@@ -0,0 +1,10 @@
+.section .init.40,"axR",@progbits
+	; make sure flags are in a sane state
+	cld
+	cli
+	; tell dodo where the software stack pointer is
+	lda #__rc0
+	sta $00
+	; Init hardware stack pointer
+	ldx #$FF
+	txs

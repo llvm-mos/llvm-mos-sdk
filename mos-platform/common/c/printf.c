911d78ed0be59113130eb7742b55ffd47a398686
Daniel Thornburgh <mysterymath@gmail.com>
2022-08-08 20:13:52 -0700

    Relicense the SDK.
    This specifically excludes files and directories for which we have not
    yet obtained permission to relicense. Hopefully, these will be sorted
    out in time.
    
    In the meantime, this change sets the expectation for new contributions
    to be the Apache License with LLVM exceptions, not the vanilla Apache
    License.

--- mos-platform/common/c/printf.c
+++ mos-platform/common/c/printf.c
@@ -1,16 +1,7 @@
 // Copyright 2022 LLVM-MOS Project
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Licensed under the Apache License, Version 2.0 with LLVM Exceptions.
+// See https://github.com/llvm-mos/llvm-mos-sdk/blob/main/LICENSE for license
+// information.
 
 ///////////////////////////////////////////////////////////////////////////////
 // \author (c) Marco Paland (info@paland.com)

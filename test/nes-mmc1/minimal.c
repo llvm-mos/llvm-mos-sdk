c586397ef67a6348145db8d12e476706896ddf88
Daniel Thornburgh <mysterymath@gmail.com>
2022-08-17 16:41:47 -0700

    Add minimal MMC1 test case using Mesen.

---
+++ test/nes-mmc1/minimal.c
@@ -0,0 +1,9 @@
+#include <peekpoke.h>
+__attribute__((noreturn)) void _exit(int status) {
+  // This is an unused APU test register, so it should be safe to use writing to
+  // this address to report the exit code to the controlling Mesen Lua script.
+  POKE(0x4018, status);
+  __builtin_unreachable();
+}
+
+int main(void) { return 0; }

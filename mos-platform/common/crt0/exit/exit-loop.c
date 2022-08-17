7ea2bd7c9de306fdf9edf41faa0dd0769211bf50
Daniel Thornburgh <mysterymath@gmail.com>
2022-08-17 16:38:16 -0700

    Make exit-loop's _exit weak; it's a fallback.

--- mos-platform/common/crt0/exit/exit-loop.c
+++ mos-platform/common/crt0/exit/exit-loop.c
@@ -1,2 +1,5 @@
-void _exit(int status) { while (1); }
-__attribute__((alias("_exit"))) void _Exit(int status);
+// An infinite loop is a fairly poor way of implementing _exit, but it's the
+// best available default. Make this weak so users can provide a better
+// application-specific implementation.
+__attribute((weak)) void _exit(int status) { while (1); }
+__attribute__((weak, alias("_exit"))) void _Exit(int status);

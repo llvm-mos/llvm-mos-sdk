a258aa487219c75bdd7941b63a65fb14766b30c6
Daniel Thornburgh <mysterymath@gmail.com>
2022-09-02 15:26:22 -0700

    Use noinit for PRG-RAM sections.

--- test/nes-nrom/prg-ram.c
+++ test/nes-nrom/prg-ram.c
@@ -1,10 +1,9 @@
-#include <stdlib.h>
 #include <peekpoke.h>
+#include <stdlib.h>
 
 asm(".global __prg_ram_size\n__prg_ram_size=8\n");
 
-__attribute__((section(".prg_ram")))
-volatile char c[8192];
+__attribute__((section(".prg_ram.noinit"))) volatile char c[8192];
 
 int main(void) {
   c[0] = 1;

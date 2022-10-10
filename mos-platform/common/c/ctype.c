c158ca9949c549551a441a2767bd8e4720d9b55d
David Given <dg@cowlark.com>
2022-10-10 20:29:17 +0200

    SDK port for CP/M-65 (#80)
    * Initial semi-working platform library.
    
    * Add elftocpm65 for converting ELF files to CP/M-65 binaries.
    
    * Add some more ctype functions.
    
    * Flesh out the BDOS API.
    
    * Cleanup some definitions.
    
    * Change most cpm_* return values to return a byte, for slightly smaller
    code.
    
    * Put cpm_fcb2 in the right place.
    
    * Generate ELF files with phdrs; use these to remove the libelf
    dependency.
    
    * Adjust the system call library to pass parameters in XA rather than
    using the C calling convention.
    
    * Move imaginary register definitions out of link.ld so the linker can
    discard them if not needed.
    
    * Move the CP/M wrapper functions into their own C file, as we can rely on
    LTO to inline them.
    
    * Allow LTO to place variables in zero page.
    
    * Remove the dependency on libfmt and getopt.
    
    * Comment and typo fix.
    
    * Rewrite the linker script completely to be better; use elftocpm65 in
    post-link-tool mode.
    
    * Add support for making BIOS calls.
    
    * Add C++ guards.

--- mos-platform/common/c/ctype.c
+++ mos-platform/common/c/ctype.c
@@ -4,3 +4,22 @@ int isprint(int c) {
   char ch = (char)c;
   return ch >= 0x1f && ch < 0x7f /*DEL*/;
 }
+
+int isdigit(int c) {
+  char ch = (char)c;
+  return ch >= '0' && ch <= '9';
+}
+
+int isalpha(int c) {
+  char ch = (char)c;
+  return ((ch >= 'A') && (ch <= 'Z'))
+    || ((ch >= 'a') && (ch <= 'z'));
+}
+
+int toupper(int c) {
+  char ch = (char)c;
+  if ((ch >= 'a') && (ch <= 'z'))
+    ch &= ~0x20;
+  return ch;
+}
+

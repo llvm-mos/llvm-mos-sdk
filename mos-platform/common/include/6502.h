051a941648d2074c2f7b40a1bb662c45d3a52d3c
Daniel Thornburgh <mysterymath@gmail.com>
2022-10-24 16:04:43 -0700

    Add 6502 instruction macros.

---
+++ mos-platform/common/include/6502.h
@@ -0,0 +1,8 @@
+#ifndef _6502_H
+#define _6502_H
+
+#define BRK() __attribute__((leaf)) asm ("brk\n")
+#define CLI() __attribute__((leaf)) asm ("cli\n")
+#define SEI() __attribute__((leaf)) asm ("sei\n")
+
+#endif // not _6502_H

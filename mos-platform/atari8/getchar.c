2f79dabfb25f91237eb1d1f2d247d84f39a83110
Piotr Fusik <fox@scene.pl>
2022-10-25 14:32:04 +0200

    Draft Atari implementation of getchar().

--- mos-platform/atari8/getchar.c
+++ mos-platform/atari8/getchar.c
@@ -1,7 +1,16 @@
+#include <atari.h>
 #include <stdio.h>
-#include <stdlib.h>
 
-int getchar() {
-  puts("getchar() not yet implemented");
-  abort();
+__attribute__((noinline)) int getchar() {
+  OS.iocb[0].command = IOCB_GETREC;
+  OS.iocb[0].buflen = 0;
+  unsigned char c;
+  asm volatile(
+    "ldx #0\n"
+    "jsr $e456\n"
+    "bmi error\n"
+    : "=a"(c) : : "p", "a", "x", "y");
+  return c == CH_EOL ? '\n' : c;
+error:
+  return EOF;
 }

711ac0a025e182b8e1ecc519f9abca05cd3b42ca
Daniel Thornburgh <mysterymath@gmail.com>
2022-11-02 09:03:49 -0700

    Add actual fixed MMC3 test; rename previous to banked-8.

--- mos-platform/nes-mmc3/ines.s
+++ mos-platform/nes-mmc3/ines.s
@@ -3,8 +3,5 @@
 .global __mapper
 __mapper = 4
 
-.weak __prg_rom_size
-__prg_rom_size = 512
-
 .weak __chr_rom_size
 __chr_rom_size = 256
---
+++ test/nes-mmc3/fixed.c
@@ -0,0 +1,19 @@
+#include <bank.h>
+#include <stdlib.h>
+
+volatile const char large_array[0x7000] = {
+    [0] = 1, [0x2000] = 2, [0x4000] = 3, [0x6000] = 4, [0x6fff] = 5};
+
+int main(void) {
+  if (large_array[0] != 1)
+    return EXIT_FAILURE;
+  if (large_array[0x2000] != 2)
+    return EXIT_FAILURE;
+  if (large_array[0x4000] != 3)
+    return EXIT_FAILURE;
+  if (large_array[0x6000] != 4)
+    return EXIT_FAILURE;
+  if (large_array[0x6fff] != 5)
+    return EXIT_FAILURE;
+  return EXIT_SUCCESS;
+}

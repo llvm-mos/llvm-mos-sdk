7c9478dd7dd2eb2522c3b0af6756de6d5f35af10
Daniel Thornburgh <mysterymath@gmail.com>
2022-11-03 09:30:35 -0700

    Fixup types.

--- mos-platform/nes-mmc3/irq.c
+++ mos-platform/nes-mmc3/irq.c
@@ -1,6 +1,6 @@
 #include "bank.h"
 
-__attribute__((section(".zp.noinit"))) void *__irq_ptr;
+__attribute__((section(".zp.noinit"))) const void *__irq_ptr;
 __attribute__((section(".zp.noinit"))) char __irq_index;
 __attribute__((section(".zp.noinit"))) char __irq_done;
 
@@ -14,6 +14,8 @@ void __disable_irq(void) {
 }
 __attribute__((weak, alias("__disable_irq"))) void disable_irq(void);
 
-__attribute__((weak)) void set_irq_ptr(void *address) { __irq_ptr = address; }
+__attribute__((weak)) void set_irq_ptr(const void *address) {
+  __irq_ptr = address;
+}
 
 __attribute__((weak)) char is_irq_done(void) { return __irq_done; }

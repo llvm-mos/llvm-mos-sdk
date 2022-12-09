aac29c0ea1a3e6b01877c49ccb6fffb47018c311
Chris Wedgwood <cw@f00f.org>
2022-12-09 11:13:38 -0800

    atari8-stdcart avoid duplicate symbols, expose _cart_init
    tweak to avoid duplicate symbols (tested with --whole-archive)
    
    allow override of _cart_init; this won't be used most of the time but
    is useful in cases where you want to taint memory in order to test
    that the data copy functions are working

---
+++ mos-platform/atari8-stdcart/copy-data.c
@@ -0,0 +1,1 @@
+../common/crt0/copy-data.c
--- mos-platform/atari8-stdcart/syms.s
+++ mos-platform/atari8-stdcart/syms.s
@@ -1,4 +1,10 @@
-; ROM size, only 8 and 16 KiB are supported
+; defaults
 
+; ROM size, only 8 and 16 KiB are supported
 .weak __cart_rom_size
 __cart_rom_size = 8
+
+; INIT typically isn't used but allow override
+.weak _cart_init
+_cart_init:
+	rts

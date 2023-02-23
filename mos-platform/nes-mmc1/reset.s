80b355d777adc50674b7e7122ce568fa69bb99cc
Daniel Thornburgh <mysterymath@gmail.com>
2023-02-23 13:48:10 -0800

    [NES] [Breaking] Make NES targets PRG-ROM LMAs accurate.
    Previously, the NES targets used a fictious notion of LMA correpsonding
    to offsets within the NES ROM image. The intended use of linker LMAs is
    to be the visible address of the ROM at start; the corresponding VMA
    is intended to be the address of the value at run time. If they differ,
    a copy must occur at start.
    
    Using fictitious LMAs meant we were fighting the linker; we needed
    complicated expressions to generate the fake LMAs, and duplicated
    sections to make sure the fake and real LMAs ligned up.
    
    Instead, we can just let LMAs be LMAs and VMAs be VMAs. We don't
    actually *need* an offset in the ROM image; the existing output format
    scripts can be generalized to accomodate the existing mappers. This
    generatlly reduces the complexity of the NES targets.
    
    As a result, there's a slight breaking change; the MMC1 mapper now uses
    prg-rom-fixed.ld in cases where the mapper's address line is
    disconnected, and prg-rom-32.ld now refers to the case where it is
    connected, but only 32KiB of PRG-ROM is available.

--- mos-platform/nes-mmc1/reset.s
+++ mos-platform/nes-mmc1/reset.s
@@ -1,15 +1,13 @@
-.macro reset bank
-  .section .reset_\bank,"ax",@progbits
-    ; Reset the mapper so the last bank appears at $c000.
-    inc $fffc
-    ; Set up the mapper for the C runtime using the code in the last bank.
-    jmp _start
-  .short $fff4
-  .short $fff4
-  .short $fff4
-.endm
+.section .reset,"ax",@progbits
+; Reset the mapper so the last bank appears at $c000.
+inc $fffc
+; Unused, since the bank has changed.
+.skip 3
+.short $fff4
+.short $fff4
+.short $fff4
 
-.section .reset_last,"ax",@progbits
+.section .reset_fixed,"ax",@progbits
 ; Reset the mapper so the last bank appears at $c000.
 inc $fffc
 ; Set up the mapper for the C runtime using the code in the last bank.
@@ -17,19 +15,3 @@ jmp _start
 .short nmi
 .short $fff4
 .short irq
-
-reset 0
-reset 1
-reset 2
-reset 3
-reset 4
-reset 5
-reset 6
-reset 7
-reset 8
-reset 9
-reset 10
-reset 11
-reset 12
-reset 13
-reset 14
---
+++ test/nes-mmc1/prg-rom-32-fixed.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <peekpoke.h>
+
+asm(".globl __prg_rom_size\n"
+    "__prg_rom_size = 32\n");
+
+volatile const char c[32000] = {1, [31999] = 2};
+
+int main(void) {
+  if (c[0] != 1)
+    return EXIT_FAILURE;
+  if (c[31999] != 2)
+    return EXIT_FAILURE;
+  return EXIT_SUCCESS;
+}

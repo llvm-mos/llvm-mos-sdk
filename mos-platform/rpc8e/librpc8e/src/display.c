6cee0c92a62821678d11728c614db9f05d4d5c13
Adrian Siekierka <kontakt@asie.pl>
2023-06-17 21:23:46 +0200

    [RPC8/e Relicense librpc8e code to Apache 2.0+LLVM

--- mos-platform/rpc8e/librpc8e/src/display.c
+++ mos-platform/rpc8e/librpc8e/src/display.c
@@ -1,25 +1,32 @@
-/**
- * Copyright (c) 2012 L. Adamson
+/*
  * Copyright (c) 2023 Adrian "asie" Siekierka
  *
- * This software is provided 'as-is', without any express or implied
- * warranty. In no event will the authors be held liable for any damages
- * arising from the use of this software.
+ * Licensed under the Apache License, Version 2.0 with LLVM Exceptions,
+ * See https://github.com/llvm-mos/llvm-mos-sdk/blob/main/LICENSE for license
+ * information.
  *
- * Permission is granted to anyone to use this software for any purpose,
- * including commercial applications, and to alter it and redistribute it
- * freely, subject to the following restrictions:
+ * Portions originally from the rpc8e-cc65 project:
+ * https://github.com/steventroughtonsmith/rpc8e-cc65/blob/master/rpc8e/conio.c
+ * Modified for LLVM-MOS SDK.
  *
- * 1. The origin of this software must not be misrepresented; you must not
- *    claim that you wrote the original software. If you use this software
- *    in a product, an acknowledgment in the product documentation would be
- *    appreciated but is not required.
+ * Copyright (C) 2012 L. Adamson
  *
- * 2. Altered source versions must be plainly marked as such, and must not be
- *    misrepresented as being the original software.
+ *  This software is provided 'as-is', without any express or implied
+ *  warranty.  In no event will the authors be held liable for any damages
+ *  arising from the use of this software.
  *
- * 3. This notice may not be removed or altered from any source distribution.
-*/
+ *  Permission is granted to anyone to use this software for any purpose,
+ *  including commercial applications, and to alter it and redistribute it
+ *  freely, subject to the following restrictions:
+ *
+ *  1. The origin of this software must not be misrepresented; you must not
+ *     claim that you wrote the original software. If you use this software
+ *     in a product, an acknowledgment in the product documentation would be
+ *     appreciated but is not required.
+ *  2. Altered source versions must be plainly marked as such, and must not be
+ *     misrepresented as being the original software.
+ *  3. This notice may not be removed or altered from any source distribution.
+ */
 
 #include <stdint.h>
 #include <rpc8e.h>

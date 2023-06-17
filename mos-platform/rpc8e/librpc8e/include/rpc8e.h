824dea2fd589495176762032f936c03e59639445
Adrian Siekierka <kontakt@asie.pl>
2023-06-17 21:44:57 +0200

    Add RPC8/e platform. (#119)
    * Add RPC8/e platform.
    
    * [RPC8/e] Add IO Expander example, target fixes
    
    * [RPC8/e] Fix soft stack handling.
    
    * [RPC8/e] Add Sortron example.
    
    * [RPC8/e] Add brief code documentation.
    
    * [RPC8/e] Add command value documentation.
    
    * [RPC8/e] Add missing .S files.
    
    * [RPC8/e] Relicense crt0 code to Apache 2.0+LLVM
    
    * [RPC8/e] Prefer old-fashioned include guards in library code.
    
    * [RPC8/e Relicense librpc8e code to Apache 2.0+LLVM

---
+++ mos-platform/rpc8e/librpc8e/include/rpc8e.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2023 Adrian "asie" Siekierka
+ *
+ * Licensed under the Apache License, Version 2.0 with LLVM Exceptions,
+ * See https://github.com/llvm-mos/llvm-mos-sdk/blob/main/LICENSE for license
+ * information.
+ */
+
+#ifndef _RPC8E_H_
+#define _RPC8E_H_
+
+#include <rpc8e/hardware.h>
+
+#ifndef __ASSEMBLER__
+#include <rpc8e/cpu.h>
+#include <rpc8e/mmu.h>
+#include <rpc8e/display.h>
+#include <rpc8e/drive.h>
+#include <rpc8e/sortron.h>
+#endif
+
+#endif /* _RPC8E_H_ */
---
+++ mos-platform/rpc8e/librpc8e/include/rpc8e/cpu.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2023 Adrian "asie" Siekierka
+ *
+ * Licensed under the Apache License, Version 2.0 with LLVM Exceptions,
+ * See https://github.com/llvm-mos/llvm-mos-sdk/blob/main/LICENSE for license
+ * information.
+ */
+
+#ifndef _RPC8E_CPU_H_
+#define _RPC8E_CPU_H_
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** \file cpu.h
+ * Functionality related to the CPU.
+ */
+
+/**
+ * @brief Emit a WAI instruction, causing the CPU to wait.
+ *
+ * On 65EL02, this means stalling for 1 world tick (0.05 seconds).
+ */
+static inline void rpc8e_cpu_wait(void) {
+	__attribute__((leaf)) asm volatile("wai");
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _RPC8E_CPU_H_ */

9fe5e0d5cc492b2c6805c6a680d750eabd18089e
Adrian Siekierka <kontakt@asie.pl>
2023-06-18 15:48:59 +0200

    [PCE] Clean up bank.h/.c/.S after figuring out the banking system.

--- mos-platform/pce/libpce/src/bank-c.c
+++ mos-platform/pce/libpce/src/bank-c.c
@@ -20,7 +20,6 @@ void pce_bank0_set(uint8_t id) {
     __attribute__((leaf)) asm volatile(
         "tam #1" : "+a"(id) : : );
 }
-void pce_bank0i_set(uint8_t id) __attribute__((alias ("pce_bank0_set")));
 void pce_bank0_size1_set(uint8_t id) __attribute__((alias ("pce_bank0_set")));
 void pce_bank0_size1i_set(uint8_t id) __attribute__((alias ("pce_bank0_set")));
 
@@ -35,7 +34,6 @@ void pce_bank1_set(uint8_t id) {
     __attribute__((leaf)) asm volatile(
         "tam #2" : "+a"(id) : : );
 }
-void pce_bank1i_set(uint8_t id) __attribute__((alias ("pce_bank1_set")));
 void pce_bank1_size1_set(uint8_t id) __attribute__((alias ("pce_bank1_set")));
 void pce_bank1_size1i_set(uint8_t id) __attribute__((alias ("pce_bank1_set")));
 
@@ -50,7 +48,6 @@ void pce_bank2_set(uint8_t id) {
     __attribute__((leaf)) asm volatile(
         "tam #4" : "+a"(id) : : );
 }
-void pce_bank2i_set(uint8_t id) __attribute__((alias ("pce_bank2_set")));
 void pce_bank2_size1_set(uint8_t id) __attribute__((alias ("pce_bank2_set")));
 void pce_bank2_size1i_set(uint8_t id) __attribute__((alias ("pce_bank2_set")));
 
@@ -61,12 +58,26 @@ __attribute__((leaf)) uint8_t pce_bank3_get(void) {
     return result;
 }
 
+void pce_bank3_set(uint8_t id) {
+    __attribute__((leaf)) asm volatile(
+        "tam #8" : "+a"(id) : : );
+}
+void pce_bank3_size1_set(uint8_t id) __attribute__((alias ("pce_bank3_set")));
+void pce_bank3_size1i_set(uint8_t id) __attribute__((alias ("pce_bank3_set")));
+
 __attribute__((leaf)) uint8_t pce_bank4_get(void) {
     uint8_t result;
     __attribute__((leaf)) asm volatile(
         "tma #16" : "=a"(result) : : );
     return result;
 }
+
+void pce_bank4_set(uint8_t id) {
+    __attribute__((leaf)) asm volatile(
+        "tam #16" : "+a"(id) : : );
+}
+void pce_bank4_size1_set(uint8_t id) __attribute__((alias ("pce_bank4_set")));
+void pce_bank4_size1i_set(uint8_t id) __attribute__((alias ("pce_bank4_set")));
 
 __attribute__((leaf)) uint8_t pce_bank5_get(void) {
     uint8_t result;
@@ -75,12 +86,26 @@ __attribute__((leaf)) uint8_t pce_bank5_get(void) {
     return result;
 }
 
+void pce_bank5_set(uint8_t id) {
+    __attribute__((leaf)) asm volatile(
+        "tam #32" : "+a"(id) : : );
+}
+void pce_bank5_size1_set(uint8_t id) __attribute__((alias ("pce_bank5_set")));
+void pce_bank5_size1i_set(uint8_t id) __attribute__((alias ("pce_bank5_set")));
+
 __attribute__((leaf)) uint8_t pce_bank6_get(void) {
     uint8_t result;
     __attribute__((leaf)) asm volatile(
         "tma #64" : "=a"(result) : : );
     return result;
 }
+
+void pce_bank6_set(uint8_t id) {
+    __attribute__((leaf)) asm volatile(
+        "tam #64" : "+a"(id) : : );
+}
+void pce_bank6_size1_set(uint8_t id) __attribute__((alias ("pce_bank6_set")));
+void pce_bank6_size1i_set(uint8_t id) __attribute__((alias ("pce_bank6_set")));
 
 __attribute__((leaf)) uint8_t pce_bank7_get(void) {
     uint8_t result;
@@ -93,6 +118,5 @@ void pce_bank7_set(uint8_t id) {
     __attribute__((leaf)) asm volatile(
         "tam #128" : "+a"(id) : : );
 }
-void pce_bank7i_set(uint8_t id) __attribute__((alias ("pce_bank7_set")));
 void pce_bank7_size1_set(uint8_t id) __attribute__((alias ("pce_bank7_set")));
 void pce_bank7_size1i_set(uint8_t id) __attribute__((alias ("pce_bank7_set")));
--- mos-platform/pce/misc/generate-libpce-bank-routines.lua
+++ mos-platform/pce/misc/generate-libpce-bank-routines.lua
@@ -27,7 +27,6 @@ for i=BANK_MIN,BANK_MAX do
 	if args[1] == "h" then
 		printf("__attribute__((leaf)) uint8_t pce_bank%d_get(void);", i)
 		printf("__attribute__((leaf)) void pce_bank%d_set(uint8_t id);", i)
-		printf("__attribute__((leaf)) void pce_bank%di_set(uint8_t id);", i)
 	elseif (args[1] == "c") then
 		printf("")
 		printf("__attribute__((leaf)) uint8_t pce_bank%d_get(void) {", i)
@@ -36,21 +35,15 @@ for i=BANK_MIN,BANK_MAX do
 		printf("        \"tma #%d\" : \"=a\"(result) : : );", (1 << i))
 		printf("    return result;")
 		printf("}")
-		if (i < BANK_CHAIN_MIN) or (i > BANK_CHAIN_MAX) or (i == BANK_CHAIN_MIN and not GENERATE_ASM_1BANKS) then
-			printf("")
-			printf("void pce_bank%d_set(uint8_t id) {", i)
-			printf("    __attribute__((leaf)) asm volatile(");
-			printf("        \"tam #%d\" : \"+a\"(id) : : );", (1 << i))
-			printf("}")
-			printf("void pce_bank%di_set(uint8_t id) __attribute__((alias (\"pce_bank%d_set\")));", i, i)
-			printf("void pce_bank%d_size%d_set(uint8_t id) __attribute__((alias (\"pce_bank%d_set\")));", i, 1, i)
-			printf("void pce_bank%d_size%di_set(uint8_t id) __attribute__((alias (\"pce_bank%d_set\")));", i, 1, i)
-		end
+		printf("")
+		printf("void pce_bank%d_set(uint8_t id) {", i)
+		printf("    __attribute__((leaf)) asm volatile(");
+		printf("        \"tam #%d\" : \"+a\"(id) : : );", (1 << i))
+		printf("}")
+		printf("void pce_bank%d_size%d_set(uint8_t id) __attribute__((alias (\"pce_bank%d_set\")));", i, 1, i)
+		printf("void pce_bank%d_size%di_set(uint8_t id) __attribute__((alias (\"pce_bank%d_set\")));", i, 1, i)
 	elseif args[1] == "asm" then
-		printf(".global pce_bank%d_set", i)
-		printf(".global pce_bank%di_set", i)
-		printf(".global pce_bank%d_size%d_set", i, 1)
-		printf(".global pce_bank%d_size%di_set", i, 1)
+		-- pass
 	else
 		printf("Usage: generate-libpce-bank-routines <c|h|asm>")
 		os.exit(1)
@@ -62,23 +55,22 @@ for i=BANK_CHAIN_MIN,BANK_CHAIN_MAX do
 		local bank_offset = i
 		local bank_size = (j - i + 1)
 		if args[1] == "h" then
-			printf("__attribute__((leaf)) void pce_bank%s_set(uint8_t id);", str)
+			-- printf("__attribute__((leaf)) void pce_bank%s_set(uint8_t id);", str)
 			printf("__attribute__((leaf)) void pce_bank%si_set(uint8_t id);", str)
 		elseif args[1] == "asm" then
-			printf(".global pce_bank%s_set", str)
+			-- printf(".global pce_bank%s_set", str)
 			printf(".global pce_bank%si_set", str)
-			printf(".global pce_bank%d_size%d_set", bank_offset, bank_size)
+			-- printf(".global pce_bank%d_size%d_set", bank_offset, bank_size)
 			printf(".global pce_bank%d_size%di_set", bank_offset, bank_size)
 		end
 	end
 end
 
 if args[1] == "asm" then
-	if GENERATE_ASM_1BANKS then
+	if false and GENERATE_ASM_1BANKS then
 		printf("")
 		printf("    .section .text.bank%d, \"ax\", @progbits", BANK_CHAIN_MIN)
 		printf("pce_bank%d_set:", BANK_CHAIN_MIN)
-		printf("pce_bank%di_set:", BANK_CHAIN_MIN)
 		printf("pce_bank%d_size1_set:", BANK_CHAIN_MIN)
 		printf("pce_bank%d_size1i_set:", BANK_CHAIN_MIN)
 		printf("    tam #$%02x", 1 << BANK_CHAIN_MIN)
@@ -86,7 +78,7 @@ if args[1] == "asm" then
 	end
 
 	for i=BANK_CHAIN_MIN+1,BANK_CHAIN_MAX do
-		for k=0,1 do
+		for k=1,1 do
 			printf("")
 			local incr = (k & 1) ~= 0
 			local suffix = ""
@@ -94,8 +86,10 @@ if args[1] == "asm" then
 			local str = generate_shorthand(BANK_CHAIN_MIN, i) .. suffix
 			printf("    .section .text.bank%s, \"ax\", @progbits", str)
 			for j=BANK_CHAIN_MIN,i do
-				printf("pce_bank%s_set:", str)
-				printf("pce_bank%d_size%d%s_set:", j, (i - j + 1), suffix)
+				if (j ~= i) then
+					printf("pce_bank%s_set:", str)
+					printf("pce_bank%d_size%d%s_set:", j, (i - j + 1), suffix)
+				end
 				printf("    tam #$%02x", 1 << j)
 				if incr and (j ~= i) then
 					printf("    inc")

d61c0a58480cae34ecb137d7b803cc3f35853a50
Adrian Siekierka <kontakt@asie.pl>
2023-06-21 18:10:36 +0200

    [PCE] Adjust color-cycle.c example to use interrupts.

--- examples/pce/color-cycle.c
+++ examples/pce/color-cycle.c
@@ -11,11 +11,30 @@
 
 #include <pce.h>
 
+uint16_t ticks = 0;
+
+// Define a VDC interrupt handler.
+__attribute__((interrupt))
+void irq_vdc(void) {
+	// This also acknowledges the interrupt.
+	uint8_t status = *IO_VDC_STATUS;
+
+	if (status & VDC_FLAG_VBLANK) {
+		ticks++;
+	}
+}
+
 int main(void) {
-	static uint16_t color;
+	// Configure the VDC screen resolution.
+	pce_vdc_set_resolution(256, 240, 0);
+
+	// Enable VDC interrupts for VBlanks.
+	pce_vdc_irq_vblank_enable();
+	pce_irq_enable(IRQ_VDC);
+	pce_cpu_irq_enable();
 
 	while(1) {
 		*IO_VCE_COLOR_INDEX = 0x100;
-		*IO_VCE_COLOR_DATA = color++;
+		*IO_VCE_COLOR_DATA = (ticks >> 3);
 	}
 }

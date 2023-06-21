1243200e9d19f0916141f0bb112475f14a39cbcc
Adrian Siekierka <kontakt@asie.pl>
2023-06-21 16:48:29 +0200

    [PCE] Further unify VBank naming conventions.

--- mos-platform/pce/libpce/include/pce/bank.h
+++ mos-platform/pce/libpce/include/pce/bank.h
@@ -156,16 +156,16 @@ __attribute__((leaf)) void pce_bank56i_set(uint8_t id);
 #define PCE_BANK_SIZE(n) ((n) << 13)
 
 #define PCE_VBANK_USE_1(type, id, offset) \
-static inline void pce_vbank ## id ## _set(void) { \
+static inline void pce_ ## type ## _vbank ## id ## _set(void) { \
     __attribute__((leaf)) asm volatile ( \
-        "lda #mos8(__" #type "_vbank_" #id "_bank)\n" \
+        "lda #mos8(__" #type "_vbank" #id "_bank)\n" \
         "tam #(1 << " #offset ")\n" : : : "a", "p" ); \
 }
 
 #define PCE_VBANK_USE_N(type, id, offset, size) \
-static inline void pce_vbank ## id ## _set(void) { \
+static inline void pce_ ## type ## _vbank ## id ## _set(void) { \
     __attribute__((leaf)) asm volatile ( \
-        "lda #mos8(__" #type "_vbank_" #id "_bank)\n" \
+        "lda #mos8(__" #type "_vbank" #id "_bank)\n" \
         "jsr pce_bank" #offset "_size" #size "i_set\n" : : : "a", "p" ); \
 }
 #define PCE_VBANK_USE_2(type, id, offset) PCE_VBANK_USE_N(type, id, offset, 2)
@@ -175,24 +175,24 @@ static inline void pce_vbank ## id ## _set(void) { \
 #define PCE_VBANK_USE(type, id, offset, size) PCE_VBANK_USE_ ## size (type, id, offset)
 
 #define PCE_VBANK_DECLARE(type, id, offset, size) \
-asm(".global __" #type "_vbank_" #id "\n.global __" #type "_vbank_" #id "_size\n.equ __" #type "_vbank_" #id ", ((" #offset ") << 13)\n.equ __" #type "_vbank_" #id "_size, ((" #size ") << 13)\n")
+asm(".global __" #type "_vbank" #id "\n.global __" #type "_vbank" #id "_size\n.equ __" #type "_vbank" #id ", ((" #offset ") << 13)\n.equ __" #type "_vbank" #id "_size, ((" #size ") << 13)\n")
 
-#define PCE_VBANK_CALLBACK_DECLARE(id) \
-__attribute__((leaf, callback(1), noinline, section("text.pce_vbank" #id "_call"))) \
-void pce_vbank ## id ## _call(void (*method)(void)) { \
-    pce_vbank ## id ## _set(); \
+#define PCE_VBANK_CALLBACK_DECLARE(type, id) \
+__attribute__((leaf, callback(1), noinline, section("text.pce_" #type "_vbank" #id "_call"))) \
+void pce_ ## type ## _vbank ## id ## _call(void (*method)(void)) { \
+    pce_ ## type ## _vbank ## id ## _set(); \
     method(); \
 }
 
-#define PCE_VBANK_CALLBACK_USE(id) \
+#define PCE_VBANK_CALLBACK_USE(type, id) \
 __attribute__((leaf, callback(1))) \
-void pce_vbank ## id ## _call(void (*method)(void))
+void pce_ ## type ## _vbank ## id ## _call(void (*method)(void))
 
 #ifdef PCE_VBANK_IMPLEMENTATION
 #define PCE_ROM_VBANK_DEFINE(id, offset, size) \
 PCE_VBANK_DECLARE(rom, id, offset, size); \
 PCE_VBANK_USE(rom, id, offset, size) \
-PCE_VBANK_CALLBACK_DECLARE(id)
+PCE_VBANK_CALLBACK_DECLARE(rom, id)
 #define PCE_ROM_FIXED_BANK_SIZE(size) \
 PCE_VBANK_DECLARE(rom, 0, 8 - size, size)
 #define PCE_SGX_RAM(size) \
@@ -213,8 +213,8 @@ asm(".global __ram_bank_size\n.equ __ram_bank_size, ((" #size ") << 13)\n")
  * - the pce_vbankN_set() function, mapping the requested virtual bank,
  * - the pce_vbankN_call(void (*method)(void)) function, allowing a safe
  *   trampoline to a function in another bank,
- * - the __rom_vbank_N section,
- * - the __rom_vbank_N_bank symbol, allowing referencing the first bank
+ * - the __rom_vbankN section,
+ * - the __rom_vbankN_bank symbol, allowing referencing the first bank
  *   of the virtual bank in assembly code.
  *
  * @param id The ID of the virtual bank.
@@ -223,7 +223,7 @@ asm(".global __ram_bank_size\n.equ __ram_bank_size, ((" #size ") << 13)\n")
  */
 #define PCE_ROM_VBANK_DEFINE(id, offset, size) \
 PCE_VBANK_USE(rom, id, offset, size) \
-PCE_VBANK_CALLBACK_USE(id)
+PCE_VBANK_CALLBACK_USE(rom, id)
 /**
  * @brief Define the size of the fixed bank ("virtual bank 0").
  * 

8f076d8838720a1afc91cda39954dd0dae96b8ae
Adrian Siekierka <kontakt@asie.pl>
2023-06-30 18:41:10 +0200

    [PCE] CD target adjustments.

--- mos-platform/pce-cd/libpce/include/pce/config.h
+++ mos-platform/pce-cd/libpce/include/pce/config.h
@@ -20,34 +20,34 @@ extern "C" {
  * Functionality related to output ELF configuration.
  */
 
-#define __PCE_CARD_BANK_DECLARE(id, offset, size) \
-asm(".global __card_bank" #id "\n.global __card_bank" #id "_size\n.equ __card_bank" #id ", ((" #offset ") << 13)\n.equ __card_bank" #id "_size, ((" #size ") << 13)\n")
+#define __PCE_RAM_BANK_DECLARE(id, offset, size) \
+asm(".global __ram_bank" #id "\n.global __ram_bank" #id "_size\n.equ __ram_bank" #id ", ((" #offset ") << 13)\n.equ __ram_bank" #id "_size, ((" #size ") << 13)\n")
 
-#define __PCE_CARD_BANK_USE(id, offset) \
-static inline void pce_card_bank ## id ## _map(void) { \
+#define __PCE_RAM_BANK_USE(id, offset) \
+static inline void pce_ram_bank ## id ## _map(void) { \
     __attribute__((leaf)) asm volatile ( \
-        "lda #mos8(__card_bank" #id "_bank)\n" \
+        "lda #mos8(__ram_bank" #id "_bank)\n" \
         "tam #(1 << " #offset ")\n" : : : "a", "p" ); \
 }
 
-#define __PCE_CARD_BANK_CALLBACK_DECLARE(id, offset) \
-__attribute__((leaf, callback(1), noinline, section("text.pce_card_bank" #id "_call"))) \
-void pce_card_bank ## id ## _call(void (*method)(void)) { \
+#define __PCE_RAM_BANK_CALLBACK_DECLARE(id, offset) \
+__attribute__((leaf, callback(1), noinline, section("text.pce_ram_bank" #id "_call"))) \
+void pce_ram_bank ## id ## _call(void (*method)(void)) { \
     pce_bank ## offset ## _size1_push(); \
-    pce_card_bank ## id ## _map(); \
+    pce_ram_bank ## id ## _map(); \
     method(); \
     pce_bank ## offset ## _size1_pop(); \
 }
 
-#define __PCE_CARD_BANK_CALLBACK_USE(id) \
+#define __PCE_RAM_BANK_CALLBACK_USE(id) \
 __attribute__((leaf, callback(1))) \
-void pce_card_bank ## id ## _call(void (*method)(void))
+void pce_ram_bank ## id ## _call(void (*method)(void))
 
 #ifdef PCE_CONFIG_IMPLEMENTATION
-#define PCE_CARD_BANK_AT(id, offset) \
-__PCE_CARD_BANK_DECLARE(id, offset, 1); \
-__PCE_CARD_BANK_USE(id, offset) \
-__PCE_CARD_BANK_CALLBACK_DECLARE(id, offset)
+#define PCE_RAM_BANK_AT(id, offset) \
+__PCE_RAM_BANK_DECLARE(id, offset, 1); \
+__PCE_RAM_BANK_USE(id, offset) \
+__PCE_RAM_BANK_CALLBACK_DECLARE(id, offset)
 #define PCE_CDB_USE_PSG_DRIVER(value) \
 asm(".global __pce_cdb_use_psg\n.equ __pce_cdb_use_psg, " #value "\n")
 #define PCE_CDB_USE_GRAPHICS_DRIVER(value) \
@@ -60,22 +60,22 @@ asm(".global __pce_cdb_use_graphics\n.equ __pce_cdb_use_graphics, " #value "\n")
  * allocated by the linker.
  *
  * The recommended way to define virtual banks is to create a header file
- * (for example, "bank.h") with PCE_CARD_BANK_AT entries, and a source
+ * (for example, "bank.h") with PCE_RAM_BANK_AT entries, and a source
  * file ("bank.c") which defines PCE_CONFIG_IMPLEMENTATION, then
  * includes <pce.h>, then includes "bank.h".
  *
  * A bank N defined in this manner provides:
- * - the pce_card_bankN_map() function, mapping the requested virtual bank,
- * - the pce_card_bankN_call(void (*method)(void)) function defines a safe
+ * - the pce_ram_bankN_map() function, mapping the requested virtual bank,
+ * - the pce_ram_bankN_call(void (*method)(void)) function defines a safe
  *   trampoline to a function in another bank,
- * - the __card_bankN section.
+ * - the __ram_bankN section.
  *
  * @param id The ID of the physical bank (0-127).
  * @param offset The memory offset, in 8KB units (2-6).
  */
-#define PCE_CARD_BANK_AT(id, offset) \
-__PCE_CARD_BANK_USE(id, offset) \
-__PCE_CARD_BANK_CALLBACK_USE(id)
+#define PCE_RAM_BANK_AT(id, offset) \
+__PCE_RAM_BANK_USE(id, offset) \
+__PCE_RAM_BANK_CALLBACK_USE(id)
 /**
  * @brief Set to 1 to enable using the BIOS PSG driver. This reserves
  * additional console RAM (6 bytes ZP, 838 bytes RAM).

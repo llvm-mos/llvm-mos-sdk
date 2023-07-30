c2f07e09bfe9a015287f9d2b288e60baacac0249
Xark <Xark@XarkLabs.com>
2023-07-29 19:16:43 -0700

    remove cbm rdtim and add cx16 rdtim (not same ABI)

---
+++ mos-platform/cx16/cx16_k_rdtim.s
@@ -0,0 +1,13 @@
+.text
+
+;
+; unsigned long cx16_k_rdtim (void);
+;
+.global cx16_k_rdtim
+cx16_k_rdtim:
+	jsr __RDTIM	; kernal returns A/X/Y, llvm wants A/X/__rc2/0
+	stz __rc2+1	; zero high byte
+	sty __rc2	; A = H
+			; X = M
+			; A = L
+	rts

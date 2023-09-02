0eb169619c92c65460a58c018454011b60812d36
Daniel Thornburgh <mysterymath@gmail.com>
2023-09-02 16:05:58 -0700

    Quote clang filename in Windows .bat wrappers
    This broke the Windows SDK if installed to a path containing spaces.
    Fixes #178

--- cmake/install-clang-batch-file.cmake
+++ cmake/install-clang-batch-file.cmake
@@ -4,5 +4,5 @@
 function(install_clang_batch_file bindir platform suffix)
   set(file mos-${platform}-${suffix}.bat)
   message(STATUS "Creating redirecting batch file ${file}")
-  file(WRITE ${bindir}/${file} "@ECHO OFF\n%~dp0\\mos-${suffix} --config mos-${platform}.cfg %*\n")
+  file(WRITE ${bindir}/${file} "@ECHO OFF\n\"%~dp0\\mos-${suffix}\" --config mos-${platform}.cfg %*\n")
 endfunction()

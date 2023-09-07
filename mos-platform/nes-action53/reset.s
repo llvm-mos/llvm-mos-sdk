109095924f4f49351b0ab8d6a2ded7e78b84f66a
James Rowe <jroweboy@gmail.com>
2023-09-07 00:23:20 -0400

    fixup! address review concern (#184)

--- mos-platform/nes-action53/reset.s
+++ mos-platform/nes-action53/reset.s
@@ -1,6 +1,9 @@
 
-.section .reset_stub,"ax",@progbits
-_reset_stub:
+.section .prg_rom_fixed.reset_stub,"ax",@progbits
+.weak reset_stub
+.globl __reset_stub
+reset_stub:
+__reset_stub:
 ; Set the $81 register for the PRG outer bank so that we bank $8000
     lda #$81
     sta $5000
@@ -12,5 +15,5 @@ _reset_stub:
 
 .section .vectors,"ax",@progbits
 .short nmi
-.short _reset_stub
+.short reset_stub
 .short irq

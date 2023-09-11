2ad037ba4a935a3eacaa02cb98600d9040edda6a
Adrian Siekierka <kontakt@asie.pl>
2023-09-11 16:59:07 +0200

    UNROM-512 target: apply feedback

--- mos-platform/nes-unrom-512/bank.c
+++ mos-platform/nes-unrom-512/bank.c
@@ -5,40 +5,50 @@
 
 #include "bank.h"
 
-extern char _BANK;
+extern volatile char _BANK;
 __attribute__((section(".zp.bss"))) char _BANK_SHADOW;
 
-__attribute__((leaf)) inline char get_bank_state(char mask) {
+__attribute__((leaf)) inline char get_bank_state(void) {
+  return _BANK_SHADOW;
+}
+
+__attribute__((leaf)) inline char set_bank_state(char value) {
+  char old = _BANK_SHADOW;
+  _BANK = (_BANK_SHADOW = value);
+  return old;
+}
+
+__attribute__((leaf)) inline char get_bank_bits(char mask) {
   return _BANK_SHADOW & mask;
 }
 
-__attribute__((leaf)) inline char set_bank_state(char mask, char value) {
+__attribute__((leaf)) inline char set_bank_bits(char mask, char value) {
   char old = _BANK_SHADOW;
   _BANK = (_BANK_SHADOW = (old & (~mask)) | value);
   return old;
 }
 
 __attribute__((leaf)) char get_prg_bank(void) {
-  return get_bank_state(UNROM_512_PRG_BANK_MASK);
+  return get_bank_bits(PRG_BANK_MASK);
 }
 
 __attribute__((leaf)) char set_prg_bank(char value) {
-  return set_bank_state(UNROM_512_PRG_BANK_MASK, value);
+  return set_bank_bits(PRG_BANK_MASK, value);
 }
 
 __attribute__((leaf)) inline char get_chr_bank(void) {
-  return get_bank_state(UNROM_512_CHR_BANK_MASK) >> UNROM_512_CHR_BANK_SHIFT;
+  return get_bank_bits(CHR_BANK_MASK) >> CHR_BANK_SHIFT;
 }
 
 __attribute__((leaf)) inline char set_chr_bank(char bank_id) {
-  return set_bank_state(UNROM_512_CHR_BANK_MASK, bank_id << UNROM_512_CHR_BANK_SHIFT);
+  return set_bank_bits(CHR_BANK_MASK, bank_id << CHR_BANK_SHIFT);
 }
 
 __attribute__((leaf)) inline char get_mirrored_screen(void) {
-  return get_bank_state(UNROM_512_MIRROR_SCREEN_MASK) >> UNROM_512_MIRROR_SCREEN_SHIFT;
+  return get_bank_bits(MIRROR_SCREEN_MASK) >> MIRROR_SCREEN_SHIFT;
 }
 
 __attribute__((leaf)) inline char set_mirrored_screen(char screen_id) {
-  return set_bank_state(UNROM_512_MIRROR_SCREEN_MASK, screen_id << UNROM_512_MIRROR_SCREEN_SHIFT);
+  return set_bank_bits(MIRROR_SCREEN_MASK, screen_id << MIRROR_SCREEN_SHIFT);
 }
 
--- mos-platform/nes-unrom-512/bank.h
+++ mos-platform/nes-unrom-512/bank.h
@@ -13,19 +13,19 @@ extern "C" {
 /**
  * @brief Define this in a .c file to use 2-screen horizontal mirroring.
  */
-#define UNROM_512_USE_2_SCREEN_HORIZONTAL \
+#define MAPPER_USE_2_SCREEN_HORIZONTAL \
 	asm(".globl __mirroring\n.globl __four_screen\n__mirroring = 0\n__four_screen = 0\n")
 
 /**
  * @brief Define this in a .c file to use 2-screen vertical mirroring.
  */
-#define UNROM_512_USE_2_SCREEN_VERTICAL \
+#define MAPPER_USE_2_SCREEN_VERTICAL \
 	asm(".globl __mirroring\n.globl __four_screen\n__mirroring = 1\n__four_screen = 0\n")
 
 /**
  * @brief Define this in a .c file to use 1-screen switchable mirroring.
  */
-#define UNROM_512_USE_1_SCREEN \
+#define MAPPER_USE_1_SCREEN \
 	asm(".globl __mirroring\n.globl __four_screen\n__mirroring = 0\n__four_screen = 1\n")
 
 /**
@@ -34,40 +34,54 @@ extern "C" {
  *
  * In this mode, only banks 0-2 of CHR-RAM are fully safe to use for pattern table data.
  */
-#define UNROM_512_USE_4_SCREEN_NAMETABLE \
+#define MAPPER_USE_4_SCREEN_NAMETABLE \
 	asm(".globl __mirroring\n.globl __four_screen\n__mirroring = 1\n__four_screen = 1\n")
 
-#define UNROM_512_PRG_BANK_MASK       0x1F
-#define UNROM_512_PRG_BANK_SHIFT      0
-#define UNROM_512_CHR_BANK_MASK       0x60
-#define UNROM_512_CHR_BANK_SHIFT      5
-#define UNROM_512_CHR_BANK(n)	      ((n) << 5)
-#define UNROM_512_CHR_BANK_0	      CHR_BANK(0)
-#define UNROM_512_CHR_BANK_1	      CHR_BANK(1)
-#define UNROM_512_CHR_BANK_2	      CHR_BANK(2)
-#define UNROM_512_CHR_BANK_3	      CHR_BANK(3)
-#define UNROM_512_MIRROR_SCREEN_MASK  0x80
-#define UNROM_512_MIRROR_SCREEN_SHIFT 7
-#define UNROM_512_MIRROR_SCREEN_1     0x00
-#define UNROM_512_MIRROR_SCREEN_2     0x80
+#define PRG_BANK_MASK       0x1F
+#define PRG_BANK_SHIFT      0
+#define CHR_BANK_MASK       0x60
+#define CHR_BANK_SHIFT      5
+#define CHR_BANK(n)	      ((n) << 5)
+#define CHR_BANK_0	      CHR_BANK(0)
+#define CHR_BANK_1	      CHR_BANK(1)
+#define CHR_BANK_2	      CHR_BANK(2)
+#define CHR_BANK_3	      CHR_BANK(3)
+#define MIRROR_SCREEN_MASK  0x80
+#define MIRROR_SCREEN_SHIFT 7
+#define MIRROR_SCREEN_1     0x00
+#define MIRROR_SCREEN_2     0x80
 
 /**
  * @brief Get the value of the bank state.
  * 
- * @param mask The mask to get the value by.
- * @return The masked bank state value.
+ * @return The current bank state.
  */
-__attribute__((leaf)) char get_bank_state(char mask);
+__attribute__((leaf)) char get_bank_state(void);
 
 /**
  * @brief Set the value of the bank state.
  * 
+ * @param value The value to set the bank state to.
+ * @return The previous bank state.
+ */
+__attribute__((leaf)) char set_bank_state(char value);
+
+/**
+ * @brief Get the value of some bits of the bank state.
+ *
+ * @param mask The mask to get the value by.
+ * @return The masked bank value.
+ */
+__attribute__((leaf)) char get_bank_bits(char mask);
+
+/**
+ * @brief Set the value of some bits of the bank state.
+ *
  * @param mask The mask to set the value by.
  * @param value The value to set. Assumed to be masked.
- * @return The previous bank state value.
+ * @return The complete previous bank state.
  */
-__attribute__((leaf)) inline char set_bank_state(char mask, char value);
-
+__attribute__((leaf)) char set_bank_bits(char mask, char value);
 
 /**
  * @brief Call a function from the given $8000-$BFFF PRG bank.
@@ -110,7 +124,7 @@ __attribute__((leaf)) char set_chr_bank(char bank_id);
 
 /**
  * @brief Get the current mirrored screen.
- * This is only effective under @ref UNROM_512_USE_1_SCREEN
+ * This is only effective under @ref MAPPER_USE_1_SCREEN .
  * 
  * @return The current mirrored screen (0-1).
  */
@@ -118,7 +132,7 @@ __attribute__((leaf)) char get_mirrored_screen(void);
 
 /**
  * @brief Switch to the given mirrored screen.
- * This is only effective under @ref UNROM_512_USE_1_SCREEN
+ * This is only effective under @ref MAPPER_USE_1_SCREEN .
  *
  * @param screen_id The mirrored screen to switch to (0-1).
  * @return The previous bank state.

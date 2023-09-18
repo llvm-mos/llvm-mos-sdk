f15710ed713786f217a5ec270da009a5ef3ff34b
Matthew Orlando <maorlando@gmail.com>
2023-09-18 11:25:44 -0700

    Fix mmc3 c-in-prg-ram uninitialized vars

---
+++ mos-platform/nes-mmc3/init-c-in-prg-ram.s
@@ -0,0 +1,9 @@
+; Copyright 2022 LLVM-MOS Project
+; Licensed under the Apache License, Version 2.0 with LLVM Exceptions.
+; See https://github.com/llvm-mos/llvm-mos-sdk/blob/main/LICENSE for license
+; information.
+
+; Set WRAM to read/write
+.section .init.30,"axR",@progbits
+lda #$80
+sta $a001

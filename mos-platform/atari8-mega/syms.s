09033fd8137d3709bd24705914e3fe3fc0d6247c
Chris Wedgwood <cw@f00f.org>
2023-12-25 01:58:25 -0800

    replace hard-coded bytes with assembly

--- mos-platform/atari8-mega/syms.s
+++ mos-platform/atari8-mega/syms.s
@@ -1,8 +1,4 @@
+;;  see sixfix.s
 
-; the default INIT (which can be overriden) is a stub to enable
-; operation of SIC! cartridges, overriding it should be done with
-; caution.
-
-.text
 .weak _cart_init
 _cart_init = 0xbffa - 14

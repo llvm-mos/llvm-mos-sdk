93de9fb8265d1ad562cb0522d24198e67c4aa725
Daniel Thornburgh <mysterymath@gmail.com>
2024-01-14 11:27:06 -0800

    [NFC] Rewrite imaxdiv in terms of lldiv

--- mos-platform/common/c/inttypes.c
+++ mos-platform/common/c/inttypes.c
@@ -1,10 +1,11 @@
 #include <inttypes.h>
 
-intmax_t imaxabs(intmax_t j) {
-  return __builtin_llabs(j);
-}
+#include <stdlib.h>
+
+intmax_t imaxabs(intmax_t j) { return __builtin_llabs(j); }
 
 imaxdiv_t imaxdiv(intmax_t numer, intmax_t denom) {
-  imaxdiv_t result = {numer / denom, numer % denom};
-  return result;
+  lldiv_t llr = lldiv(numer, denom);
+  imaxdiv_t r = {llr.quot, llr.rem};
+  return r;
 }

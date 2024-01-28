8caf39b2f0ec178534c46b7527168920263afb28
rumbledethumps <16963588+rumbledethumps@users.noreply.github.com>
2024-01-27 17:36:48 -0800

    fix long return

--- mos-platform/rp6502/lrand.c
+++ mos-platform/rp6502/lrand.c
@@ -4,10 +4,5 @@ long lrand(void) {
   RIA.op = RIA_OP_LRAND;
   while (RIA.busy & RIA_BUSY_BIT)
     ;
-  long l = RIA.a;
-  l += RIA.x << 8;
-  l += RIA.sreg << 16;
-  return l;
-  // This doesn't work
-  // return RIA.a + (RIA.x << 8) + (RIA.sreg << 16);
+  return ((long)RIA.sreg << 16) + (RIA.x << 8) + RIA.a;
 }

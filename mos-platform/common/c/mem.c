543b5a9f9e71e2afcb4239b9b293546323f7c2a0
Daniel Thornburgh <mysterymath@gmail.com>
2024-01-29 21:38:39 -0800

    [Breaking] Finish <string.h>
    - Implemented strcspn and strstr
    - Added all __restrict__ modifiers from the standard
    - Renamed all parameters and reordered functions to match the standard
    - Breaking: renamed strrev to _strrev, since it's not a formal part of
      the standard (and to match current Microsoft recommendations.)

--- mos-platform/common/c/mem.c
+++ mos-platform/common/c/mem.c
@@ -1,4 +1,51 @@
+#include <stdint.h>
 #include <string.h>
+
+// Copying functions
+
+__attribute__((weak)) void *memcpy(void *__restrict__ s1,
+                                   const void *__restrict__ s2, size_t n) {
+  char *d;
+  const char *s;
+  for (d = s1, s = s2; n; d++, s++, --n)
+    *d = *s;
+  return s1;
+}
+
+__attribute__((weak)) void *memmove(void *s1, const void *s2, size_t n) {
+  uintptr_t us1 = (uintptr_t)s1;
+  uintptr_t us2 = (uintptr_t)s2;
+  if (us1 <= us2)
+    return memcpy(s1, s2, n);
+
+  // Don't add -1 to s1 or s2; this is undefined behavior.
+  if (!n)
+    return s1;
+  char *d = s1 + n - 1;
+  const char *s = s2 + n - 1;
+
+  while (1) {
+    *d = *s;
+    // Don't decrement d or s past the beginning of the object; this is
+    // undefined behavior.
+    if (!--n)
+      return s1;
+    --d, --s;
+  }
+  return s1;
+}
+
+// Comparison functions
+
+__attribute__((weak)) int memcmp(const void *s1, const void *s2, size_t n) {
+  const char *a, *b;
+  for (a = s1, b = s2; n; ++a, ++b, --n)
+    if (*a != *b)
+      return *a - *b;
+  return 0;
+}
+
+// Search functions
 
 __attribute__((weak)) void *memchr(const void *s, int c, size_t n) {
   const char *sc;
@@ -9,21 +56,7 @@ __attribute__((weak)) void *memchr(const void *s, int c, size_t n) {
   return NULL;
 }
 
-__attribute__((weak)) int memcmp(const void *s1, const void *s2, size_t n) {
-  const char *a, *b;
-  for (a = s1, b = s2; n; ++a, ++b, --n)
-    if (*a != *b)
-      return *a - *b;
-  return 0;
-}
-
-__attribute__((weak)) void *memcpy(void *dest, const void *src, size_t count) {
-  char *d;
-  const char *s;
-  for (d = dest, s = src; count; d++, s++, --count)
-    *d = *s;
-  return dest;
-}
+// Miscellaneous functions
 
 __attribute__((weak)) void *memset(void *ptr, int value, size_t num) {
   __memset((char *)ptr, (char)value, num);
@@ -33,27 +66,4 @@ __attribute__((weak)) void *memset(void *ptr, int value, size_t num) {
 __attribute__((weak)) void __memset(char *ptr, char value, size_t num) {
   for (; num; ptr++, num--)
     *ptr = value;
-}
-
-__attribute__((weak)) void *memmove(void *dest, const void *src, size_t num) {
-  unsigned int udest = (unsigned int)dest;
-  unsigned int usrc = (unsigned int)src;
-  if (udest <= usrc)
-    return memcpy(dest, src, num);
-
-  // Don't add -1 to dest or src; this is undefined behavior.
-  if (!num)
-    return dest;
-  char *d = dest + num - 1;
-  const char *s = src + num - 1;
-
-  while (1) {
-    *d = *s;
-    // Don't decrement d or s past the beginning of the object; this is
-    // undefined behavior.
-    if (!--num)
-      return dest;
-    --d, --s;
-  }
-  return dest;
 }
--- mos-platform/common/include/string.h
+++ mos-platform/common/include/string.h
@@ -8,32 +8,42 @@
 extern "C" {
 #endif
 
-void *memchr(const void *s, int c, size_t n);
-int memcmp(const void *s1, const void *s2, size_t n);
-void *memcpy(void *dest, const void *src, size_t count);
-void *memset(void *ptr, int value, size_t num);
-void *memmove(void *dest, const void *src, size_t num);
-
-char *strchr(const char *s, int c);
-
-int strcmp(const char *s1, const char *s2);
-
+// Copying functions
+void *memcpy(void *__restrict__ s1, const void *__restrict__ s2, size_t n);
+void *memmove(void *s1, const void *s2, size_t n);
 char *strcpy(char *__restrict__ s1, const char *__restrict__ s2);
-
-const char *strerror(int n);
-
-size_t strlen(const char *s);
-int strncmp(const char *s1, const char *s2, size_t n);
 char *strncpy(char *__restrict__ s1, const char *__restrict__ s2, size_t n);
 
-char *strrchr(const char *s, int c);
-char *strrev(char *str);
-size_t strspn(const char *string, const char *in);
-char *strpbrk(const char *string, const char *brk);
-char *strtok(char *string, const char *separators);
+// Concatenation functions
 
 char *strcat(char *__restrict__ s1, const char *__restrict__ s2);
 char *strncat(char *__restrict__ s1, const char *__restrict__ s2, size_t n);
+
+// Comparison functions
+
+int memcmp(const void *s1, const void *s2, size_t n);
+int strcmp(const char *s1, const char *s2);
+int strncmp(const char *s1, const char *s2, size_t n);
+
+// Search functions
+
+void *memchr(const void *s, int c, size_t n);
+char *strchr(const char *s, int c);
+size_t strcspn(const char *s1, const char *s2);
+char *strpbrk(const char *s1, const char *s2);
+char *strrchr(const char *s, int c);
+size_t strspn(const char *s1, const char *s2);
+char *strstr(const char *s1, const char *s2);
+char *strtok(char *__restrict__ s1, const char *__restrict__ s2);
+
+// Miscellaneous functions
+
+void *memset(void *ptr, int value, size_t num);
+const char *strerror(int n);
+
+size_t strlen(const char *s);
+
+char *_strrev(char *str);
 
 // Version of memset with better arguments for MOS. All non-pointer arguments
 // can fit in registers, and there is no superfluous return value. Compiler

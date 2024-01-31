1488250651a39986354d51399b234c69007937c1
Daniel Thornburgh <mysterymath@gmail.com>
2024-01-31 13:59:03 -0800

    Rewrite printf using PDCLib.
    This removes the MIT-licensed mpaland embedded print in favor of a new
    one based on PDCLib. This removes an annoying license gaff from the SDK,
    and it changes the following:
    
    - Floating point printing is now correctly rounded.
    - %a is now supported.
    - Non-float printf is smaller and faster:
      - Simulator printf("Hello %d\n", 6502)
        - Before
          - Size:   6520
          - Cycles: 8865
        - After
          - Size:   4251
          - Cycles: 6160
    - float printf is much smaller but slower:
      - Simulator printf("Hello %f\n", 6502.0)
        - Before
          - Size:   40482
          - Cycles: 41425
        - After
          - Size:   9546
          - Cycles: 218678
    
    Note that the floating point printing routines target correctness first
    (since essentially mandated by the standard), then size, then speed. We
    could probably speed them up in common cases without adding too much
    more size. It's difficult to go smaller than the current implementation.

--- mos-platform/common/include/stdio.h
+++ mos-platform/common/include/stdio.h
@@ -3,38 +3,6 @@
 // See https://github.com/llvm-mos/llvm-mos-sdk/blob/main/LICENSE for license
 // information.
 
-///////////////////////////////////////////////////////////////////////////////
-// \author (c) Marco Paland (info@paland.com)
-//             2014-2019, PALANDesign Hannover, Germany
-//
-// \license The MIT License (MIT)
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
-//
-// \brief Tiny printf, sprintf and snprintf implementation, optimized for speed
-// on
-//        embedded systems with a very limited resources.
-//        Use this instead of bloated standard/newlib printf.
-//        These routines are thread safe and reentrant.
-//
-///////////////////////////////////////////////////////////////////////////////
-
 #ifndef _STDIO_H_
 #define _STDIO_H_
 
@@ -45,56 +13,41 @@
 extern "C" {
 #endif
 
-/**
- * Tiny printf implementation
- * \param format A string that specifies the format of the output
- * \return The number of characters that are written into the array, not
- * counting the terminating null character
- */
-int printf(const char *format, ...);
+// Originally from the Public Domain C Library (PDCLib).
 
-/**
- * Tiny sprintf implementation
- * Due to security reasons (buffer overflow) YOU SHOULD CONSIDER USING
- * (V)SNPRINTF INSTEAD! \param buffer A pointer to the buffer where to store the
- * formatted string. MUST be big enough to store the output! \param format A
- * string that specifies the format of the output \return The number of
- * characters that are WRITTEN into the buffer, not counting the terminating
- * null character
- */
-int sprintf(char *buffer, const char *format, ...);
-
-/**
- * Tiny snprintf/vsnprintf implementation
- * \param buffer A pointer to the buffer where to store the formatted string
- * \param count The maximum number of characters to store in the buffer,
- * including a terminating null character \param format A string that specifies
- * the format of the output \param va A value identifying a variable arguments
- * list \return The number of characters that COULD have been written into the
- * buffer, not counting the terminating null character. A value equal or larger
- * than count indicates truncation. Only when the returned value is non-negative
- * and less than count, the string has been completely written.
- */
-int snprintf(char *buffer, size_t count, const char *format, ...);
-int vsnprintf(char *buffer, size_t count, const char *format, va_list va);
-
-/**
- * Tiny vprintf implementation
- * \param format A string that specifies the format of the output
- * \param va A value identifying a variable arguments list
- * \return The number of characters that are WRITTEN into the buffer, not
- * counting the terminating null character
- */
-int vprintf(const char *format, va_list va);
-
-int putchar(int c);
-int puts(const char *s);
+struct _FILE;
+typedef struct _FILE FILE;
 
 #define EOF (-1)
 
-// Begin platform-specific backend.
+extern FILE *stdin;
+extern FILE *stdout;
+extern FILE *stderr;
 
+// Formated input/output functions
+
+int fprintf(FILE *__restrict__ stream, const char *__restrict__ format, ...);
+int printf(const char *__restrict__ format, ...);
+int snprintf(char *__restrict__ s, size_t n, const char *__restrict__ format,
+             ...);
+int sprintf(char *__restrict__ s, const char *__restrict__ format, ...);
+int vfprintf(FILE *__restrict__ stream, const char *__restrict__ format,
+             va_list arg);
+int vprintf(const char *__restrict__ format, va_list arg);
+int vsnprintf(char *__restrict__ s, size_t n, const char *__restrict__ format,
+              va_list arg);
+int vsprintf(char *__restrict__ s, const char *__restrict__ format,
+             va_list arg);
+
+// Character input/output functions
+
+int fputc(int c, FILE *stream);
+int fputs(const char *__restrict__ s, FILE *__restrict__ stream);
 int getchar(void);
+int putchar(int c);
+int puts(const char *s);
+
+// Platform-specific backend
 
 // Emit a sequence of characters in the target's character set that
 // correspond to the given ASCII character.
@@ -103,29 +56,6 @@ __attribute__((always_inline)) void __char_conv(char c, void (*emit)(char c));
 // Put a character already in the target's character set out to the target's
 // equivalent of file descriptor 0.
 void __putchar(char c);
-
-// End platform-specific backend.
-
-// Originally from the Public Domain C Library (PDCLib).
-
-struct _FILE;
-typedef struct _FILE FILE;
-
-extern FILE *stdin;
-extern FILE *stdout;
-extern FILE *stderr;
-
-/**
- * Write the value c (cast to unsigned char) to the given stream.
- * \return c if successful, EOF otherwise.
- */
-int fputc(int c, FILE *stream);
-
-/**
- * Write the string s (not including the terminating \0) to the given stream.
- * \return A value >=0 if successful, EOF otherwise.
- */
-int fputs(const char *__restrict__ s, FILE *__restrict__ stream);
 
 #ifdef __cplusplus
 }

72e337959d136a5971ad1c9c2eee7e933df0f2b2
Daniel Thornburgh <mysterymath@gmail.com>
2024-01-31 09:41:02 -0800

    [Breaking] Use shifted PETSCII by default
    The spirit of the C standard is the implementation do whatever
    translation is necessary to members of the execution character set to
    have them stored or displayed properly on the target system. For
    commodore targets, this really does mean that we should follow CC65 and
    use the shifted character set.
    
    Since our execution character set is always ASCII, we do need to swap
    uppercase and lowercase characters at runtime. While the code to do this
    isn't *that* horrible, its overhead is the main reason we haven't done
    thus until now.
    
    However, preliminary design work for stdio provided a way to do this by
    default, while still allowing the user to turn it off. The default
    __char_conv implementation for commodore does this translation and pulls
    in a constructor that establishes shifted PETSCII at program start. If
    the user provides their own __char_conv, the constructor will never be
    pulled in, and they can use their own compile-time mechanisms to ensure
    the printed strings are already in PETSCII as appropriate.

---
+++ mos-platform/commodore/char-conv.c
@@ -0,0 +1,26 @@
+#include <ctype.h>
+
+#define __CBM__
+#include <cbm.h>
+
+// The default __char_conv when used with the screen requires the screen to be
+// in shifted PETSCII, so establish it before it is used. This is done in
+// assembly to avoid pulling in init_array machinery for small programs.
+asm(".section .init.250,\"axR\",@progbits\n"
+    "shift:\n"
+    "  lda #0x0e\n"
+    "  jsr __CHROUT\n");
+
+__attribute__((always_inline, weak)) void __char_conv(char c,
+                                                      void (*emit)(char c)) {
+  if (__builtin_expect(c == '\n', 0)) {
+    emit('\r');
+    return;
+  }
+  if ('a' <= c && c <= 'z')
+    emit(c & ~0x20);
+  else if ('A' <= c && c <= 'Z')
+    emit(c | 0x80);
+  else
+    emit(c);
+}
--- mos-platform/commodore/putchar.c
+++ mos-platform/commodore/putchar.c
@@ -1,13 +1,6 @@
-#define __CBM__
-#include <cbm.h>
 #include <stdio.h>
 
-__attribute__((always_inline, weak)) void __char_conv(char c,
-                                                      void (*emit)(char c)) {
-  if (__builtin_expect(c == '\n', 0))
-    emit('\r');
-  else
-    emit(c);
-}
+#define __CBM__
+#include <cbm.h>
 
 void __putchar(char c) { cbm_k_chrout(c); }

fb4f7c324569b7673c2f110a59a74dcff43874fa
Mikael Lund <mlund@mac.com>
2024-02-13 09:57:00 +0000

    Update comment

--- mos-platform/common/c/math.cc
+++ mos-platform/common/c/math.cc
@@ -1,14 +1,14 @@
 #include <math.h>
 
 template <typename T> static inline T _fmax(const T x, const T y) {
-  // if x is nan, then x > y is false, so the ternary returns y
   if (isnan(y)) return x;
+  // if x is nan, then x > y is false, so the ternary returns y
   return x > y ? x : y;
 }
 
 template <typename T> static inline T _fmin(const T x, const T y) {
-  // if x is nan, then x > y is false, so the ternary returns y
   if (isnan(y)) return x;
+  // if x is nan, then x < y is false, so the ternary returns y
   return x < y ? x : y;
 }
 

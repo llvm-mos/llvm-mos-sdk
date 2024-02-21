8a21f3403247ef12b9aa0f7cec41b9f76c247cef
Daniel Thornburgh <mysterymath@gmail.com>
2024-02-20 19:15:59 -0800

    [Breaking] Switch to ISO mode for CX16
    The spirit and letter of the C standard state that the effects of
    printing printable characters in the execution character set (ASCII)
    should produce the corresponding character on the display. Since the
    Commander X16 has full ASCII support, we need to switch to ISO mode if a
    character could ever be output to the screen. This is similar to the
    shifted PETSCII switch now done for other Commodore family targets.

---
+++ mos-platform/cx16/char-conv.c
@@ -0,0 +1,20 @@
+#define __CBM__
+#include <cbm.h>
+
+// The default __char_conv when used with the screen requires the screen to be
+// in ISO mode, so establish it before it is used. This is done in
+// assembly to avoid pulling in init_array machinery for small programs.
+asm(".section .init.250,\"axR\",@progbits\n"
+    "shift:\n"
+    "  lda #0x0f\n"
+    "  jsr __CHROUT\n");
+
+__attribute__((always_inline, weak)) void __char_conv(char c,
+                                                      void (*emit)(char c)) {
+  if (__builtin_expect(c == '\n', 0))
+    emit('\r');
+  else if (__builtin_expect(c == '\b', 0))
+    emit('\x9d'); // CURSOR LEFT
+  else
+    emit(c);
+}

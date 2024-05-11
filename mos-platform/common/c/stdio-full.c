26f8f68f6453fde86d89902025285f2e28c997ac
Daniel Thornburgh <mysterymath@gmail.com>
2024-05-10 18:38:16 -0700

    Import fwrite from PDCLib

--- mos-platform/common/c/stdio-full.c
+++ mos-platform/common/c/stdio-full.c
@@ -338,9 +338,69 @@ size_t fread(void *__restrict ptr, size_t size, size_t nmemb,
   __stdio_not_yet_implemented();
 }
 
-size_t fwrite(const void *__restrict ptr, size_t size, size_t nmemb,
-              FILE *__restrict__ stream) {
-  __stdio_not_yet_implemented();
+size_t fwrite(const void *restrict ptr, size_t size, size_t nmemb,
+              FILE *restrict stream) {
+  size_t offset = 0;
+  size_t nmemb_i;
+
+  if (prepwrite(stream) == EOF)
+    return 0;
+
+  for (nmemb_i = 0; nmemb_i < nmemb; ++nmemb_i) {
+    for (size_t size_i = 0; size_i < size; ++size_i) {
+      if ((stream->buffer[stream->bufidx++] =
+               ((char *)ptr)[nmemb_i * size + size_i]) == '\n') {
+        /* Remember last newline, in case we have to do a partial line-buffered
+         * flush */
+        offset = stream->bufidx;
+      }
+
+      if (stream->bufidx == stream->bufsize) {
+        if (flush_buffer(stream) == EOF) {
+          /* Returning number of objects completely buffered */
+          return nmemb_i;
+        }
+
+        offset = 0;
+      }
+    }
+  }
+
+  /* Fully-buffered streams are OK. Non-buffered streams must be flushed,
+     line-buffered streams only if there's a newline in the buffer.
+  */
+  switch (stream->status & (_IONBF | _IOLBF)) {
+  case _IONBF:
+    if (flush_buffer(stream) == EOF) {
+      /* We are in a pinch here. We have an error, which requires a
+         return value < nmemb. On the other hand, all objects have
+         been written to buffer, which means all the caller had to
+         do was removing the error cause, and re-flush the stream...
+         Catch 22. We'll return a value one short, to indicate the
+         error, and can't really do anything about the inconsistency.
+      */
+      return nmemb_i - 1;
+    }
+
+    break;
+
+  case _IOLBF:
+    if (offset > 0) {
+      size_t bufidx = stream->bufidx;
+      stream->bufidx = offset;
+
+      if (flush_buffer(stream) == EOF) {
+        /* See comment above. */
+        stream->bufidx = bufidx;
+        return nmemb_i - 1;
+      }
+
+      stream->bufidx = bufidx - offset;
+      memmove(stream->buffer, stream->buffer + offset, stream->bufidx);
+    }
+  }
+
+  return nmemb_i;
 }
 
 // File positioning functions

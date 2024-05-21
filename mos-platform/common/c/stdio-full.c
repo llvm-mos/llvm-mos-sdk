2a32f01590714e9f7e32a33b21456aaae871aabd
Daniel Thornburgh <mysterymath@gmail.com>
2024-05-21 14:25:18 -0700

    Implement ftell from PDCLib

--- mos-platform/common/c/stdio-full.c
+++ mos-platform/common/c/stdio-full.c
@@ -838,7 +838,35 @@ int fsetpos(FILE *stream, const fpos_t *pos) {
   return 0;
 }
 
-long int ftell(FILE *stream) { __stdio_not_yet_implemented(); }
+long int ftell(FILE *stream) {
+  /* ftell() must take into account:
+     - the actual *physical* offset of the file, i.e. the offset as recognized
+       by the operating system (and stored in stream->pos.offset); and
+     - any buffers held by PDCLib, which
+       - in case of unwritten buffers, count in *addition* to the offset; or
+       - in case of unprocessed pre-read buffers, count in *substraction* to
+         the offset. (Remember to count ungetidx into this number.)
+     Conveniently, the calculation ( ( bufend - bufidx ) + ungetidx ) results
+     in just the right number in both cases:
+       - in case of unwritten buffers, ( ( 0 - unwritten ) + 0 )
+         i.e. unwritten bytes as negative number
+       - in case of unprocessed pre-read, ( ( preread - processed ) + unget )
+         i.e. unprocessed bytes as positive number.
+     That is how the somewhat obscure return-value calculation works.
+  */
+  /*  If offset is too large for return type, report error instead of wrong
+      offset value.
+  */
+  long int rc;
+  if ((stream->pos - stream->bufend) >
+      (LONG_MAX - (stream->bufidx - (int)stream->ungetc_buf_full))) {
+    /* integer overflow */
+    errno = ERANGE;
+    return -1;
+  }
+  return (stream->pos - (((int)stream->bufend - (int)stream->bufidx) +
+                         (int)stream->ungetc_buf_full));
+}
 
 void rewind(FILE *stream) { __stdio_not_yet_implemented(); }
 

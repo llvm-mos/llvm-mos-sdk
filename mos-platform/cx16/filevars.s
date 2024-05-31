a83c611ab557d5e80ffd06c70612193e4bdae2c2
Daniel Thornburgh <mysterymath@gmail.com>
2024-05-31 13:38:16 -0700

    Add missing commodore devnum/filevars
    This allows e.g. commodore fopen to link on non-c64 targets.

---
+++ mos-platform/cx16/filevars.s
@@ -0,0 +1,38 @@
+; Copyright 2024 LLVM-MOS Project
+; Licensed under the Apache License, Version 2.0 with LLVM Exceptions.
+; See https://github.com/llvm-mos/llvm-mos-sdk/blob/main/LICENSE for license
+; information.
+
+; Originally from cc65. Modified from original version.
+
+; 2002-11-15, Ullrich von Bassewitz
+; 2019-11-08, Greg King
+;
+; Variables used for CBM file I/O
+;
+
+        .globl         curunit
+
+        .include        "cx16.inc"
+
+
+.bss
+curunit:
+        .fill    1
+
+
+.section .init,"axR",@progbits
+initcurunit:
+        lda     DEVNUM
+        bne     L0
+        lda     #8              ; Default is SD card
+        sta     DEVNUM
+L0:     sta     curunit
+        rts
+
+
+.section .fini,"axR",@progbits
+updatedevnum:
+        lda     curunit
+        sta     DEVNUM
+        rts
---
+++ mos-platform/mega65/filevars.s
@@ -0,0 +1,25 @@
+; Copyright 2024 LLVM-MOS Project
+; Licensed under the Apache License, Version 2.0 with LLVM Exceptions.
+; See https://github.com/llvm-mos/llvm-mos-sdk/blob/main/LICENSE for license
+; information.
+
+; Originally from cc65. Modified from original version.
+
+;
+; Ullrich von Bassewitz, 15.11.2002
+;
+; Variables used for Mega65 file I/O
+;
+
+        .globl         curunit
+
+
+.bss
+curunit:
+        .fill    1
+
+
+.section .init,"axR",@progbits
+initcurunit:
+        lda     #8              ; Default is disk
+        sta     curunit

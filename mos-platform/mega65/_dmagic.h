b4878a7efd54ef5969b5b00cbb2a9cf41f55a08a
Mikael Lund <Mlund@mac.com>
2024-07-29 23:51:20 +0200

    Prettify doxygen strings

--- mos-platform/mega65/_dmagic.h
+++ mos-platform/mega65/_dmagic.h
@@ -45,11 +45,10 @@ enum
   DMA_HOLD = 16,      //!< Do not change the address (bit 4)
   DMA_MODULO = 32,    //!< Apply the MODULO field to wrap around (bit 5)
   DMA_DIRECTION = 64, //!< Apply the MODULO field to wrap around (bit 6)
-  DMA_IO = 128, //!< I/O registers visible during the DMA controller at
-                       //!< $D000–$DFFF (bit 7).
+  DMA_IO = 128, //!< I/O registers visible during the DMA controller at $D000–$DFFF (bit 7).
 };
 
-/// DMA options
+/// DMA options (incomplete)
 enum
 #ifdef __clang__
     : uint8_t
@@ -72,9 +71,9 @@ struct DMAAudioChannel {
   uint8_t enable; //!< Enable Audio DMA channel X (offset 0x00)
   union {
     struct {
-      uint8_t baddr_lsb; //!< base address LSB (offset 0x01)
-      uint8_t baddr_msb; //!< base address MSB (offset 0x02)
-      uint8_t baddr_mb;  //!< base address middle byte (offset 0x03)
+      uint8_t baddr_lsb; //!< Base address LSB (offset 0x01)
+      uint8_t baddr_msb; //!< Base address MSB (offset 0x02)
+      uint8_t baddr_mb;  //!< Base address middle byte (offset 0x03)
     };
 #ifdef __clang__
     unsigned _BitInt(24) baddr; //!< 24-bit base address (offset 0x01)
@@ -82,9 +81,9 @@ struct DMAAudioChannel {
   };
   union {
     struct {
-      uint8_t freq_lsb; //!< frequency LSB (offset 0x04)
-      uint8_t freq_mb;  //!< frequency middle byte (offset 0x05)
-      uint8_t freq_msb; //!< frequency MSB (offset 0x06)
+      uint8_t freq_lsb; //!< Frequency LSB (offset 0x04)
+      uint8_t freq_mb;  //!< Frequency middle byte (offset 0x05)
+      uint8_t freq_msb; //!< Frequency MSB (offset 0x06)
     };
 #ifdef __clang__
     unsigned _BitInt(24) freq; //!< 24-bit frequency (offset 0x04)
@@ -92,8 +91,8 @@ struct DMAAudioChannel {
   };
   union {
     struct {
-      uint8_t taddr_lsb; //!< top address LSB (offset 0x07)
-      uint8_t taddr_msb; //!< top address MSB (offset 0x08)
+      uint8_t taddr_lsb; //!< Top address LSB (offset 0x07)
+      uint8_t taddr_msb; //!< Top address MSB (offset 0x08)
     };
 #ifdef __clang__
     unsigned _BitInt(16) taddr; //!< 16-bit top address (offset 0x07)
@@ -102,9 +101,9 @@ struct DMAAudioChannel {
   uint8_t volume; //!< playback volume (offset 0x09)
   union {
     struct {
-      uint8_t curaddr_lsb; //!< current address LSB (offset 0x0a)
-      uint8_t curaddr_mb;  //!< current address middle byte (offset 0x0b)
-      uint8_t curaddr_msb; //!< current address MSB (offset 0x0c)
+      uint8_t curaddr_lsb; //!< Current address LSB (offset 0x0a)
+      uint8_t curaddr_mb;  //!< Current address middle byte (offset 0x0b)
+      uint8_t curaddr_msb; //!< Current address MSB (offset 0x0c)
     };
 #ifdef __clang__
     unsigned _BitInt(24) curaddr; //!< 24-bit current address (offset 0x0a)
@@ -112,9 +111,9 @@ struct DMAAudioChannel {
   };
   union {
     struct {
-      uint8_t tmraddr_lsb; //!< timing counter LSB (offset 0x0d)
-      uint8_t tmraddr_mb;  //!< timing counter middle byte (offset 0x0e)
-      uint8_t tmraddr_msb; //!< timing counter MSB (offset 0x0f)
+      uint8_t tmraddr_lsb; //!< Timing counter LSB (offset 0x0d)
+      uint8_t tmraddr_mb;  //!< Timing counter middle byte (offset 0x0e)
+      uint8_t tmraddr_msb; //!< Timing counter MSB (offset 0x0f)
     };
 #ifdef __clang__
     unsigned _BitInt(24) tmraddr; //!< 24-bit timing counter (offset 0x0d)
@@ -148,7 +147,7 @@ enum
 {
   DMA_CHENABLE = 0b10000000,   //!< Enable channel
   DMA_CHLOOP = 0b01000000,     //!< Channel looping
-  DMA_CHSGN = 0b00100000,      //!< If set, use unsigned samples
+  DMA_CHSGN = 0b00100000,      //!< Sample sign bit: 0=signed, 1=unsigned.
   DMA_CHSINE = 0b00010000,     //!< Play 32-sample sine wave instead of DMA data
   DMA_CHSTP = 0b00001000,      //!< Stop flag
   DMA_CHSBITS_16 = 0b00000011, //!< 16-bit samples
@@ -160,19 +159,19 @@ struct DMAgicController {
   uint8_t addr_lsb_trigger; //!< DMAgic DMA list address LSB, and trigger DMA (when written) (offset 0x00)
   uint8_t addr_msb;         //!< DMA list address high byte (address bits 8 – 15) (offset 0x01)
   uint8_t addr_bank;        //!< DMA list address bank (address bits 16 – 22). Writing clears $D704 (offset 0x02)
-  uint8_t enable_f018b;     //!< offset 0x03, extensed fields
+  uint8_t enable_f018b;     //!< Offset 0x03, extensed fields
   uint8_t addr_mb;          //!< DMA list address mega-byte (offset 0x04)
   uint8_t trigger_enhanced; //!< Set low-order byte of DMA list address, and trigger Enhanced DMA job (offset 0x05)
   uint8_t etrigmapd;        //!< Set low-order byte of DMAlistaddress and trigger EnhancedDMA job, with list in current CPU memory map (offset 0x06)
-  uint8_t unused1[7];       //!< offset 0x07-0x0d
+  uint8_t unused1[7];       //!< Offset 0x07-0x0d
   uint8_t addr_lsb;         //!< DMA list address low byte (address bits 0 – 7) WITHOUT STARTING A DMA JOB (offset 0x0e)
-  uint8_t unused2[2];       //!< offset 0x0f-0x10
+  uint8_t unused2[2];       //!< Offset 0x0f-0x10
   uint8_t auden;            //!< Enable Audio DMA (offset 0x11)
-  uint8_t unused3[10];      //!< offset 0x12-0x1b
-  uint8_t ch0rvol;          //!< Audio DMA channel 0 right channel volume (offset 0x1c)
-  uint8_t ch1rvol;          //!< Audio DMA channel 1 right channel volume (offset 0x1d)
-  uint8_t ch2lvol;          //!< Audio DMA channel 2 left channel volume (offset 0x1e)
-  uint8_t ch3lvol;          //!< Audio DMA channel 3 left channel volume (offset 0x1f)
+  uint8_t unused3[10];      //!< Offset 0x12-0x1b
+  uint8_t ch0rvol;          //!< Channel 0 right channel volume (offset 0x1c)
+  uint8_t ch1rvol;          //!< Channel 1 right channel volume (offset 0x1d)
+  uint8_t ch2lvol;          //!< Channel 2 left channel volume (offset 0x1e)
+  uint8_t ch3lvol;          //!< Channel 3 left channel volume (offset 0x1f)
   union {
     struct {
       struct DMAAudioChannel ch0; //!< Audio DMA channel 0 (offset 0x20)
@@ -190,25 +189,25 @@ static_assert(sizeof(DMAgicController) == 0x60);
 
 /// Older 11 byte DMA list structure; also known as just "F018"
 struct DMAList_F018A {
-  uint8_t command;      //!< offset 0x00
-  uint16_t count;       //!< offset 0x01
-  uint16_t source_addr; //!< offset 0x03
-  uint8_t source_bank;  //!< offset 0x05
-  uint16_t dest_addr;   //!< offset 0x06
-  uint8_t dest_bank;    //!< offset 0x08
-  uint16_t modulo;      //!< offset 0x09
+  uint8_t command;      //!< Offset 0x00
+  uint16_t count;       //!< Offset 0x01
+  uint16_t source_addr; //!< Offset 0x03
+  uint8_t source_bank;  //!< Offset 0x05
+  uint16_t dest_addr;   //!< Offset 0x06
+  uint8_t dest_bank;    //!< Offset 0x08
+  uint16_t modulo;      //!< Offset 0x09
 };
 
 /// Newer 12-byte "F018B" DMA list structure
 struct DMAList_F018B {
-  uint8_t command;      //!< offset 0x00
-  uint16_t count;       //!< offset 0x01
-  uint16_t source_addr; //!< offset 0x03
-  uint8_t source_bank;  //!< offset 0x05
-  uint16_t dest_addr;   //!< offset 0x06
-  uint8_t dest_bank;    //!< offset 0x08
-  uint8_t command_msb;  //!< offset 0x09
-  uint16_t modulo;      //!< offset 0x0a
+  uint8_t command;      //!< Offset 0x00
+  uint16_t count;       //!< Offset 0x01
+  uint16_t source_addr; //!< Offset 0x03
+  uint8_t source_bank;  //!< Offset 0x05
+  uint16_t dest_addr;   //!< Offset 0x06
+  uint8_t dest_bank;    //!< Offset 0x08
+  uint8_t command_msb;  //!< Offset 0x09
+  uint16_t modulo;      //!< Offset 0x0a
 };
 
 #endif

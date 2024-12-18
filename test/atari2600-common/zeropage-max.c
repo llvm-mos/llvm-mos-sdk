8cdb7ce1f4d41d7dec28ec9cb4e2cb7f5fe8cbda
Daniel Thornburgh <mysterymath@gmail.com>
2024-12-18 09:28:26 -0800

    Leave a less strict version of the zeropage-max test

--- test/atari2600-common/zeropage-max.c
+++ test/atari2600-common/zeropage-max.c
@@ -7,24 +7,13 @@
 
 // 128 bytes PIA RAM
 // 32 bytes for registers
-// 16 bytes for stack
+// 16 bytes for stack (overestimate)
 // 8 bytes for test result
 char buf[128 - 32 - 16 - 8];
-
-void test_recurse(int n) {
-  if (n == 0) {
-    return;
-  }
-  buf[0]++;
-  test_recurse(n - 1);
-  buf[0]--;
-}
 
 void test_zp_max() {
   // set memory
   memset(buf, 0x55, sizeof(buf));
-  // test 10 layers of recursion
-  test_recurse(10);
   // compare memory
   for (int i = 0; i < sizeof(buf); i++) {
     if (buf[i] != 0x55) {

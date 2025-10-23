590739eec94c0d639c21899a3c0d60c9d61e1abf
rumbledethumps <16963588+rumbledethumps@users.noreply.github.com>
2025-10-22 18:50:53 -0700

    f_getcwd

--- mos-platform/rp6502/f_getcwd.c
+++ mos-platform/rp6502/f_getcwd.c
@@ -4,8 +4,12 @@
 
 int f_getcwd(char *name, int size) {
   int i, ax;
-  ria_set_ax(size);
   ax = ria_call_int(RIA_OP_GETCWD);
+  if (ax > size) {
+    RIA.op = RIA_OP_ZXSTACK;
+    errno = ENOMEM;
+    return -1;
+  }
   for (i = 0; i < ax; i++) {
     name[i] = ria_pop_char();
   }

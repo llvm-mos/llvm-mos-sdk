c2054d638f7b53b5fe78ccff038c2f005ee9e7da
Essem <smswessem@gmail.com>
2025-10-27 16:42:45 -0500

    fds: Change NMI_ACTION_USER to NMI_ACTION_USER_1

--- mos-platform/fds/irq.h
+++ mos-platform/fds/irq.h
@@ -14,12 +14,12 @@ void fds_set_timer_irq_reload(unsigned value);
 void fds_set_timer_irq_ctrl(bool enabled, bool repeat);
 
 #define NMI_ACTION_VINTWAIT 0b00000000
-#define NMI_ACTION_USER 0b01000000
+#define NMI_ACTION_USER_1 0b01000000
 #define NMI_ACTION_USER_2 0b10000000
 #define NMI_ACTION_USER_3 0b11000000
 
 // Set action on NMI. Should be one of NMI_ACTION_VINTWAIT,
-// NMI_ACTION_USER, NMI_ACTION_USER_2, or NMI_ACTION_USER_3.
+// NMI_ACTION_USER_1, NMI_ACTION_USER_2, or NMI_ACTION_USER_3.
 void fds_set_nmi_action(char action);
 
 #define IRQ_ACTION_DISK_SKIP(bytes) bytes & 0b00111111

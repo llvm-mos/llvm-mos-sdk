1d1c8d6f11fe7c2a2e50c0d35d9783dce825155f
Essem <smswessem@gmail.com>
2025-10-28 21:10:08 -0500

    fds: Replace skip bytes macro with documentation

--- mos-platform/fds/irq.h
+++ mos-platform/fds/irq.h
@@ -22,13 +22,14 @@ void fds_set_timer_irq_ctrl(bool enabled, bool repeat);
 // NMI_ACTION_USER_1, NMI_ACTION_USER_2, or NMI_ACTION_USER_3.
 void fds_set_nmi_action(char action);
 
-#define IRQ_ACTION_DISK_SKIP(bytes) bytes & 0b00111111
 #define IRQ_ACTION_DISK_TRANSFER 0b01000000
 #define IRQ_ACTION_ACK_DELAY 0b10000000
 #define IRQ_ACTION_USER 0b11000000
 
-// Set action on IRQ. Should be one of IRQ_ACTION_DISK_SKIP,
-// IRQ_ACTION_DISK_TRANSFER, IRQ_ACTION_ACK_DELAY, or IRQ_ACTION_USER.
+// Set action on IRQ. Should be one of IRQ_ACTION_DISK_TRANSFER,
+// IRQ_ACTION_ACK_DELAY, or IRQ_ACTION_USER. Alternatively, a 6-bit
+// value can be passed to use the "skip bytes" action, which will
+// skip the given amount of bytes on disk.
 void fds_set_irq_action(char action);
 
 #define RESET_TYPE_HARD 0xAC
